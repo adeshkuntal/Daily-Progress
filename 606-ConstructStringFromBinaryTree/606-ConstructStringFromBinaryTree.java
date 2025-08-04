7b324162fc9a12136bdec8acab1c9a64e3fb309c
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2025-08-04 18:55:48 +0530

    Create 606-ConstructStringFromBinaryTree

---
+++ 606-ConstructStringFromBinaryTree/606-ConstructStringFromBinaryTree.java
@@ -0,0 +1,33 @@
+// Last updated: 8/4/2025, 6:54:26 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public String tree2str(TreeNode root) {
+        if (root == null) return "";
+
+        String left = tree2str(root.left);
+        String right = tree2str(root.right);
+
+        if (left.equals("") && right.equals("")) {
+            return root.val + "";
+        }
+        if (right.equals("")) {
+            return root.val + "(" + left + ")";
+        }
+
+        return root.val + "(" + left + ")(" + right + ")";
+    }
+}
