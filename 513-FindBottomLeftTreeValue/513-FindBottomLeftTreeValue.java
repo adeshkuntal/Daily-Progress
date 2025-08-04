3cec58f87045e258b99b8f78ade4ccf767335c7f
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2025-08-04 18:56:14 +0530

    Create 513-FindBottomLeftTreeValue

---
+++ 513-FindBottomLeftTreeValue/513-FindBottomLeftTreeValue.java
@@ -0,0 +1,36 @@
+// Last updated: 8/4/2025, 6:54:52 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int leftmost = root.val;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            leftmost = queue.peek().val; // first node in this level
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+            }
+        }
+
+        return leftmost;
+    }
+}
