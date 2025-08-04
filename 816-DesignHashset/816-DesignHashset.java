c73a034fed7177cc2c34843533295bde6ab97abe
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2025-08-04 18:54:47 +0530

    Create 816-DesignHashset

---
+++ 816-DesignHashset/816-DesignHashset.java
@@ -0,0 +1,35 @@
+// Last updated: 8/4/2025, 6:53:24 PM
+import java.util.*;
+
+class MyHashSet {
+    private final int SIZE = 1000;
+    private LinkedList<Integer>[] buckets;
+
+    public MyHashSet() {
+        buckets = new LinkedList[SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            buckets[i] = new LinkedList<>();
+        }
+    }
+
+    private int hash(int key) {
+        return key % SIZE;
+    }
+
+    public void add(int key) {
+        int index = hash(key);
+        if (!buckets[index].contains(key)) {
+            buckets[index].add(key);
+        }
+    }
+
+    public void remove(int key) {
+        int index = hash(key);
+        buckets[index].remove(Integer.valueOf(key));
+    }
+
+    public boolean contains(int key) {
+        int index = hash(key);
+        return buckets[index].contains(key);
+    }
+}
