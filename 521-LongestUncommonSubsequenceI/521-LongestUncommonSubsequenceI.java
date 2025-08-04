27b82684859bc5f996855c76dc3a07a49ec1f52d
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2025-08-04 18:56:11 +0530

    Create 521-LongestUncommonSubsequenceI

---
+++ 521-LongestUncommonSubsequenceI/521-LongestUncommonSubsequenceI.java
@@ -0,0 +1,9 @@
+// Last updated: 8/4/2025, 6:54:48 PM
+class Solution {
+    public int findLUSlength(String a, String b) {
+        if (a.equals(b)) {
+            return -1;
+        }
+        return Math.max(a.length(), b.length());
+    }
+}
