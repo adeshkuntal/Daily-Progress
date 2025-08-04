47b7f3c4c60ad529e7eb8ccf94eb606682fdf4e5
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2025-08-04 18:57:14 +0530

    Create 343-IntegerBreak

---
+++ 343-IntegerBreak/343-IntegerBreak.java
@@ -0,0 +1,14 @@
+// Last updated: 8/4/2025, 6:55:51 PM
+class Solution {
+    public int integerBreak(int n) {
+        if (n == 2) return 1;
+        if (n == 3) return 2;
+        
+        if (n % 3 == 0)
+            return (int)Math.pow(3, n / 3);
+        else if (n % 3 == 1)
+            return (int)Math.pow(3, (n / 3) - 1) * 4;
+        else
+            return (int)Math.pow(3, n / 3) * 2;
+    }
+}
