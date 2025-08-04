2864d6963b31e8630252b54a58b6f250c49abb9e
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2025-08-04 18:57:42 +0530

    Create 263-UglyNumber

---
+++ 263-UglyNumber/263-UglyNumber.java
@@ -0,0 +1,15 @@
+// Last updated: 8/4/2025, 6:56:19 PM
+class Solution {
+    public boolean isUgly(int n) {
+       if (n <= 0) return false;
+        
+        int[] primes = {2, 3, 5};
+        for (int prime : primes) {
+            while (n % prime == 0) {
+                n /= prime;
+            }
+        }
+        
+        return n == 1;
+    }
+}
