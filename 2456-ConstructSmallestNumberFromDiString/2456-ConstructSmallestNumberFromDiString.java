13872002711396967afdb41afbcbcae81ff2c574
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2025-08-04 18:50:56 +0530

    Create 2456-ConstructSmallestNumberFromDiString

---
+++ 2456-ConstructSmallestNumberFromDiString/2456-ConstructSmallestNumberFromDiString.java
@@ -0,0 +1,26 @@
+// Last updated: 8/4/2025, 6:49:33 PM
+class Solution {
+    public String smallestNumber(String str) {
+        int[] ans =new int[str.length()+1];
+        int c=1;
+        Stack<Integer> st=new Stack<>();
+        for(int i=0;i<=str.length();i++){
+            if(i==str.length() || str.charAt(i)=='I'){
+                ans[i]=c;
+                c++;
+                while(!st.isEmpty()){
+                    ans[st.pop()]=c;
+                    c++;
+                }
+            }
+            else{
+                st.push(i);
+            }
+        }
+        String s="";
+        for(int v:ans){
+            s+=v;
+        }
+        return s; 
+    }
+}
