66a26e31261ecaadddf307c9e0039a0c5e79d140
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2025-08-04 18:53:04 +0530

    Create 1274-NumberOfDaysBetweenTwoDates

---
+++ 1274-NumberOfDaysBetweenTwoDates/1274-NumberOfDaysBetweenTwoDates.java
@@ -0,0 +1,11 @@
+// Last updated: 8/4/2025, 6:51:42 PM
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+class Solution {
+    public int daysBetweenDates(String date1, String date2) {
+        LocalDate d1 = LocalDate.parse(date1);
+        LocalDate d2 = LocalDate.parse(date2);
+        return (int) Math.abs(ChronoUnit.DAYS.between(d1, d2));
+        
+    }
+}
