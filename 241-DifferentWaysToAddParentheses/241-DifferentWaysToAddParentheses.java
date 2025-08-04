0eae9037f00522b5ee9179a440691b73d04b400a
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2025-08-04 18:57:49 +0530

    Create 241-DifferentWaysToAddParentheses

---
+++ 241-DifferentWaysToAddParentheses/241-DifferentWaysToAddParentheses.java
@@ -0,0 +1,32 @@
+// Last updated: 8/4/2025, 6:56:26 PM
+class Solution {
+    public List<Integer> diffWaysToCompute(String expression) {
+        List<Integer> result = new ArrayList<>();
+        
+        for (int i = 0; i < expression.length(); i++) {
+            char ch = expression.charAt(i);
+            
+            if (ch == '+' || ch == '-' || ch == '*') {
+                String left = expression.substring(0, i);
+                String right = expression.substring(i + 1);
+                
+                List<Integer> leftResults = diffWaysToCompute(left);
+                List<Integer> rightResults = diffWaysToCompute(right);
+                
+                for (int l : leftResults) {
+                    for (int r : rightResults) {
+                        if (ch == '+') result.add(l + r);
+                        else if (ch == '-') result.add(l - r);
+                        else result.add(l * r);
+                    }
+                }
+            }
+        }
+        
+        if (result.isEmpty()) {
+            result.add(Integer.parseInt(expression));
+        }
+        
+        return result;
+    }
+}
