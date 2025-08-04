dd18502e95f196398dd9d7d8deaf9e8298bd71c7
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2025-08-04 18:57:00 +0530

    Create 386-LexicographicalNumbers

---
+++ 386-LexicographicalNumbers/386-LexicographicalNumbers.java
@@ -0,0 +1,24 @@
+// Last updated: 8/4/2025, 6:55:37 PM
+class Solution {
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> l=new ArrayList<>();
+
+        print(n,0,l);
+        return l;
+    }
+    public static void print(int n,int curr,List<Integer> l){
+        if(curr > n){
+            return;
+        }
+        if(curr != 0){
+             l.add(curr);
+        }
+        int i = 0;
+        if(curr == 0){
+            i=1;
+        }
+        for(; i<=9; i++){
+            print(n,curr*10+i,l);
+        }
+    }
+}
