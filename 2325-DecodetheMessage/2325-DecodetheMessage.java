e884761d6d314f69031db86c21d6971ab5572a12
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2025-08-15 11:33:57 +0530

    Create 2325-DecodetheMessage

---
+++ 2325-DecodetheMessage/2325-DecodetheMessage.java
@@ -0,0 +1,29 @@
+// Last updated: 8/15/2025, 11:32:35 AM
+class Solution {
+    public String decodeMessage(String key, String message) {
+        char[] map = new char[26];
+        boolean[] seen = new boolean[26];
+        int index = 0;
+
+        for (int i = 0; i < key.length(); i++) {
+            char c = key.charAt(i);
+            if (c != ' ' && !seen[c - 'a']) {
+                map[c - 'a'] = (char) ('a' + index);
+                seen[c - 'a'] = true;
+                index++;
+            }
+        }
+
+        StringBuilder decoded = new StringBuilder();
+        for (int i = 0; i < message.length(); i++) {
+            char c = message.charAt(i);
+            if (c == ' ') {
+                decoded.append(' ');
+            } else {
+                decoded.append(map[c - 'a']);
+            }
+        }
+
+        return decoded.toString();
+    }
+}
