bd41067112bd2ba940ffd9164866ac64e6a9cb2e
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2025-08-18 10:54:12 +0530

    Create 679-24Game

---
+++ 679-24Game/679-24Game.java
@@ -0,0 +1,31 @@
+// Last updated: 8/18/2025, 10:52:50 AM
+class Solution {
+    public boolean judgePoint24(int[] cards) {
+        double[] nums = new double[cards.length];
+        for (int i = 0; i < cards.length; i++) nums[i] = cards[i];
+        return solve(nums);
+    }
+
+    private boolean solve(double[] nums) {
+        int n = nums.length;
+        if (n == 1) return Math.abs(nums[0] - 24) < 1e-6;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i != j) {
+                    double[] next = new double[n - 1];
+                    int idx = 0;
+                    for (int k = 0; k < n; k++) if (k != i && k != j) next[idx++] = nums[k];
+                    for (double val : compute(nums[i], nums[j])) {
+                        next[n - 2] = val;
+                        if (solve(next)) return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private double[] compute(double a, double b) {
+        return new double[]{a + b, a - b, b - a, a * b, a / b, b / a};
+    }
+}
