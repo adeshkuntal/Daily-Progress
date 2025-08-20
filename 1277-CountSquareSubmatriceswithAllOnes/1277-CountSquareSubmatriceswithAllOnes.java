78afe35a6a3f3cb5e3568d5adb0b06b24092d2ac
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2025-08-20 21:25:38 +0530

    Create 1277-CountSquareSubmatriceswithAllOnes

---
+++ 1277-CountSquareSubmatriceswithAllOnes/1277-CountSquareSubmatriceswithAllOnes.java
@@ -0,0 +1,21 @@
+// Last updated: 8/20/2025, 9:24:17 PM
+class Solution {
+    public int countSquares(int[][] matrix) {
+        int count = 0;
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 1) {
+                    if (i > 0 && j > 0) {
+                        matrix[i][j] = Math.min(matrix[i - 1][j - 1],
+                                       Math.min(matrix[i - 1][j], matrix[i][j - 1])) + 1;
+                    }
+                    count += matrix[i][j];
+                }
+            }
+        }
+        return count;
+    }
+}
