5e38b80c68c552f418438a65de6c507449cbbe7c
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2025-08-25 19:28:29 +0530

    File updated at 8/25/2025, 7:27:07 PM

--- 498-DiagonalTraverse/498-DiagonalTraverse.java
+++ 498-DiagonalTraverse/498-DiagonalTraverse.java
@@ -1,11 +1,14 @@
-// Last updated: 8/25/2025, 7:26:31 PM
+// Last updated: 8/25/2025, 7:27:07 PM
 class Solution {
     public int[] findDiagonalOrder(int[][] mat) {
         if (mat.length == 0) return new int[0];
         int m = mat.length;
         int n = mat[0].length;
         int[] res = new int[m * n];
-        int r = 0, c = 0, dir = 1, idx = 0;
+        int r = 0;
+        int c = 0;
+        int dir = 1;
+        int idx = 0;
         while (idx < m * n) {
             res[idx++] = mat[r][c];
             if (dir == 1) {
