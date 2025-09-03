35ee9079095479c6829051d6afb9644353560bc3
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2025-09-03 11:34:57 +0530

    Create 198-HouseRobber

---
+++ 198-HouseRobber/198-HouseRobber.java
@@ -0,0 +1,21 @@
+// Last updated: 9/3/2025, 11:33:37 AM
+import java.util.*;
+class Solution {
+    public int rob(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, -1);
+        return Robber(nums, 0, dp);
+    }
+    public int Robber(int[] nums, int i, int[] dp) {
+        if(i >= nums.length) {
+            return 0;
+        }
+        if(dp[i] != -1) {
+            return dp[i];
+        }
+        int rob = nums[i] + Robber(nums, i + 2, dp);
+        int dont_rob = Robber(nums, i + 1, dp);
+
+        return dp[i] = Math.max(rob, dont_rob);
+    }
+}
