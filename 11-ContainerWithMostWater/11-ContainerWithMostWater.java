7b84d8f2444fe6faaeeb6101d5bf53bf9f19c6b4
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2025-09-07 12:19:13 +0530

    File updated at 9/7/2025, 12:17:53 PM

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.java
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.java
@@ -1,24 +1,37 @@
-// Last updated: 9/6/2025, 8:11:23 PM
+// Last updated: 9/7/2025, 12:17:53 PM
 class Solution {
     public int maxArea(int[] height) {
+        // Time Complexity - O(n^2)
+
+        // int area = 0;
+        // for(int i=0; i<height.length; i++){
+        //     for(int j=i; j<height.length; j++){
+        //         int ans = (j-i) * Math.min(height[i],height[j]);
+        //         if(ans>area){
+        //             area = ans;
+        //         }
+        //     }
+        // }
+        // return area;
+
+
+        // Time Complexity - O(n)
         int area = 0;
-        int w = 0;
-        int h = 0;
-        int i = 0;
-        int j = height.length - 1;
-        while (i < j) {
-            w = j - i;
-            h = Math.min(height[i], height[j]);
-            int r = w * h;
-            if (area < r) {
-                area = r;
+        int left = 0;
+        int right = height.length - 1;
+        for(int i=0; i<height.length; i++){
+            int res = (right - left) * Math.min(height[left], height[right]);
+            if(res>area){
+                area = res;
             }
-            if (height[i] < height[j]) {
-                i++;
-            } else {
-                j--;
+            if(height[left]<height[right]){
+                left++;
+            }
+            else{
+                right--;
             }
         }
+
         return area;
     }
 }
