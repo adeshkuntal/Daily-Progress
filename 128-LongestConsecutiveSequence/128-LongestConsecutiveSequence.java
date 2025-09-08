5f25df816f10638da3836c9760ae1d559ebd5d4a
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2025-09-08 11:24:00 +0530

    File updated at 9/8/2025, 11:22:41 AM

--- 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.java
+++ 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.java
@@ -1,26 +1,49 @@
-// Last updated: 9/8/2025, 11:05:14 AM
+// Last updated: 9/8/2025, 11:22:41 AM
 import java.util.Arrays;
 
 class Solution {
     public int longestConsecutive(int[] nums) {
+        // Time Complexity: O(n log n)
+
+        // if(nums.length == 0) return 0;
+        // Arrays.sort(nums);
+        // int count = 1;
+        // int res = 1;
+        // int a = nums[0];
+        // for(int i = 1; i < nums.length; i++){
+        //     if(nums[i] == nums[i-1]) continue;
+        //     if(nums[i] == a + count){
+        //         count++;
+        //     }
+        //     else{
+        //         if(count > res){
+        //             res = count;
+        //         }
+        //         count = 1;
+        //         a = nums[i];
+        //     }
+        // }
+        // return Math.max(res, count);
+
+
+        // Time Complexity: O(n)
         if(nums.length == 0) return 0;
-        Arrays.sort(nums);
-        int count = 1;
+        Set<Integer> s = new HashSet<>();
+        for(int n : nums){
+            s.add(n);
+        }
         int res = 1;
-        int a = nums[0];
-        for(int i = 1; i < nums.length; i++){
-            if(nums[i] == nums[i-1]) continue;
-            if(nums[i] == a + count){
-                count++;
-            }
-            else{
+        for(int a : s){
+            if(!s.contains(a-1)){
+                int count = 1;
+                while(s.contains(a+count)){
+                    count++;
+                }
                 if(count > res){
                     res = count;
                 }
-                count = 1;
-                a = nums[i];
             }
         }
-        return Math.max(res, count);
+        return res;
     }
 }
