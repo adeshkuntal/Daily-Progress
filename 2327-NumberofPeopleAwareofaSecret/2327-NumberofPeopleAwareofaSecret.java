19a683c2f7186dbb8c787dc570fcad1fae9cddc7
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2025-09-09 20:52:13 +0530

    File updated at 9/9/2025, 8:50:54 PM

--- 2327-NumberofPeopleAwareofaSecret/2327-NumberofPeopleAwareofaSecret.java
+++ 2327-NumberofPeopleAwareofaSecret/2327-NumberofPeopleAwareofaSecret.java
@@ -1,24 +1,21 @@
-// Last updated: 9/9/2025, 8:48:47 PM
-class Solution {
+// Last updated: 9/9/2025, 8:50:54 PM
+class Solution { 
     public int peopleAwareOfSecret(int n, int delay, int forget) {
-        int MOD = 1000000007;
-        long[] dp = new long[n + 1];
-        long[] prefix = new long[n + 2];
-        dp[1] = 1;
-        prefix[2] = 1;
-
-        for (int day = 2; day <= n; day++) {
-            int start = Math.max(0, day - forget);
-            int end = Math.max(0, day - delay);
-            long val = (prefix[end + 1] - prefix[start + 1] + MOD) % MOD;
-            dp[day] = val;
-            prefix[day + 1] = (prefix[day] + dp[day]) % MOD;
+        int MOD = (int) 1e9 + 7; 
+        long[] dp = new long[n + 1]; 
+        dp[1] = 1; 
+        for (int i = 2; i <= n; i++) { 
+            int start = Math.max(1, i - forget + 1); 
+            int end = i - delay;
+            for (int j = start; j <= end; j++) { 
+                dp[i] = (dp[i] + dp[j]) % MOD;
+            }
         }
-
-        long ans = 0;
-        for (int day = n - forget + 1; day <= n; day++) {
-            if (day >= 1) ans = (ans + dp[day]) % MOD;
+        long result = 0; 
+        int start = Math.max(1, n - forget + 1); 
+        for (int i = start; i <= n; i++) { 
+            result = (result + dp[i]) % MOD;
         }
-        return (int) ans;
+        return (int) result;
     }
 }
