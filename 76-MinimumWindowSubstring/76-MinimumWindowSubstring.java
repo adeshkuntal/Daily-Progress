513cf62f9a72cf9e62a82d558f50672d72406d23
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2025-09-17 20:52:29 +0530

    File updated at 9/17/2025, 8:51:11 PM

--- 76-MinimumWindowSubstring/76-MinimumWindowSubstring.java
+++ 76-MinimumWindowSubstring/76-MinimumWindowSubstring.java
@@ -1,37 +1,44 @@
-// Last updated: 9/17/2025, 7:55:52 PM
+// Last updated: 9/17/2025, 8:51:11 PM
+import java.util.*;
+
 class Solution {
     public String minWindow(String s, String t) {
         HashMap<Character,Integer> map = new HashMap<>();
-        for (char c : t.toCharArray())
+        for (char c : t.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) + 1);
-
-        int l = 0, r = 0, subl = 0, subr = 0, formed = 0, total = map.size();
-        int lenans = Integer.MAX_VALUE;
-
-        while (r < s.length()) {
-            char c = s.charAt(r);
-            if (map.containsKey(c)) {
-                map.put(c, map.get(c) - 1);
-                if (map.get(c) == 0) formed++;
-            }
-
-            while (l <= r && formed == total) {
-                int curlen = r - l + 1;
-                if (curlen < lenans) {
-                    lenans = curlen;
-                    subl = l;
-                    subr = r + 1;
-                }
-                char leftc = s.charAt(l);
-                if (map.containsKey(leftc)) {
-                    if (map.get(leftc) == 0) formed--;
-                    map.put(leftc, map.get(leftc) + 1);
-                }
-                l++;
-            }
-            r++;
         }
-        return lenans == Integer.MAX_VALUE ? "" : s.substring(subl, subr);
+
+        int left = 0;
+        int required = map.size();
+        int formed = 0;
+        HashMap<Character,Integer> window = new HashMap<>();
+
+        int minLen = Integer.MAX_VALUE;
+        int start = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+            window.put(c, window.getOrDefault(c, 0) + 1);
+
+            if (map.containsKey(c) && window.get(c).intValue() == map.get(c).intValue()) {
+                formed++;
+            }
+
+            while (left <= right && formed == required) {
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    start = left;
+                }
+
+                char chLeft = s.charAt(left);
+                window.put(chLeft, window.get(chLeft) - 1);
+                if (map.containsKey(chLeft) && window.get(chLeft) < map.get(chLeft)) {
+                    formed--;
+                }
+                left++;
+            }
+        }
+
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
     }
 }
-
