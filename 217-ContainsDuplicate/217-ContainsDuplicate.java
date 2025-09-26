8454070e145c8f3fedec62997ef2f28d1e6819db
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2025-09-26 23:32:55 +0530

    File updated at 9/26/2025, 11:31:37 PM

--- 217-ContainsDuplicate/217-ContainsDuplicate.java
+++ 217-ContainsDuplicate/217-ContainsDuplicate.java
@@ -1,7 +1,7 @@
-// Last updated: 8/4/2025, 6:56:44 PM
+// Last updated: 9/26/2025, 11:31:37 PM
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);  // Sort the array first
+        Arrays.sort(nums);
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] == nums[i - 1]) {
                 return true;
