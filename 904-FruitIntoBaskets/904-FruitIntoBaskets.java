bf1e8d552ee5f1e2cea1fad21b0b8cf8a728381e
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2025-09-26 15:02:06 +0530

    File updated at 9/26/2025, 3:00:47 PM

--- 904-FruitIntoBaskets/904-FruitIntoBaskets.java
+++ 904-FruitIntoBaskets/904-FruitIntoBaskets.java
@@ -1,10 +1,14 @@
-// Last updated: 8/4/2025, 7:04:32 PM
+// Last updated: 9/26/2025, 3:00:47 PM
 class Solution {
     public int totalFruit(int[] fruits) {
-        int max = 0, start = 0;
+        int max = 0;
+        int start = 0;
         Map<Integer, Integer> map = new HashMap<>();
+
         for (int end = 0; end < fruits.length; end++) {
+
             map.put(fruits[end], map.getOrDefault(fruits[end], 0) + 1);
+
             while (map.size() > 2) {
                 map.put(fruits[start], map.get(fruits[start]) - 1);
                 if (map.get(fruits[start]) == 0) {
@@ -12,8 +16,10 @@ class Solution {
                 }
                 start++;
             }
+
             max = Math.max(max, end - start + 1);
         }
+        
         return max;
     }
 }
