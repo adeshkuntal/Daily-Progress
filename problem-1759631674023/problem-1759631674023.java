7d879aab650af1e01e22df636e930236a5364b9b
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2025-10-05 08:05:50 +0530

    Create

---
+++ problem-1759631674023/problem-1759631674023.java
@@ -0,0 +1,16 @@
+// Last updated: 10/5/2025, 8:04:34 AM
+class Solution {
+    public int alternatingSum(int[] nums) {
+        int sum = 0;
+        for(int i=0; i<nums.length; i++){
+            if(i%2==0){
+                sum+=nums[i];
+            }
+            else{
+                sum-=nums[i];
+            }
+        }
+
+        return sum;
+    }
+}
