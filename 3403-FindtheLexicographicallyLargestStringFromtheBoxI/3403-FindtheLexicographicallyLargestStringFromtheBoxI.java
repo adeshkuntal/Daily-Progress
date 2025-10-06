21bf7be08e8e8d1d6abb5137e0477f6499559926
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2025-10-06 22:08:13 +0530

    Create 3403-FindtheLexicographicallyLargestStringFromtheBoxI

---
+++ 3403-FindtheLexicographicallyLargestStringFromtheBoxI/3403-FindtheLexicographicallyLargestStringFromtheBoxI.java
@@ -0,0 +1,23 @@
+// Last updated: 10/6/2025, 10:06:57 PM
+class Solution {
+    public String answerString(String word, int numFriends) {
+        if (numFriends == 1) return word;
+
+        String maxP1 = "";
+        int maxLength = word.length() - numFriends + 1;
+
+        for (int i = 0; i < word.length(); i++) {
+            String temp = "";
+            if (i + maxLength <= word.length()) {
+                temp = word.substring(i, i + maxLength);
+            } else {
+                temp = word.substring(i);
+            }
+
+            if (temp.compareTo(maxP1) > 0) {
+                maxP1 = temp;
+            }
+        }
+        return maxP1;
+    }
+}
