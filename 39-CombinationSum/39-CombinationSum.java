a1d2216fe06b56432ccff85594bf0aa01b41ddb5
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2025-10-11 21:18:55 +0530

    File updated at 10/11/2025, 9:17:40 PM

--- 39-CombinationSum/39-CombinationSum.java
+++ 39-CombinationSum/39-CombinationSum.java
@@ -1,20 +1,20 @@
-// Last updated: 8/4/2025, 6:58:17 PM
+// Last updated: 10/11/2025, 9:17:40 PM
 class Solution {
-    public List<List<Integer>> combinationSum(int[] coin, int amount) {
+    public List<List<Integer>> combinationSum(int[] coin, int target) {
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> l= new ArrayList<>();
-        combination(coin,amount,l,0,ans);
+        combination(coin,target,l,0,ans);
         return ans;
     }
-    public static void combination(int[] coin,int amount,List<Integer> l,int idx,List<List<Integer>> ans){
-        if(amount==0){
+    public static void combination(int[] coin,int target,List<Integer> l,int idx,List<List<Integer>> ans){
+        if(target==0){
             ans.add(new ArrayList<>(l));
             return;
         }
         for(int i=idx; i< coin.length; i++){
-            if(amount>=coin[i]){
+            if(target>=coin[i]){
                 l.add(coin[i]);
-                combination(coin,amount-coin[i],l,i,ans);
+                combination(coin,target-coin[i],l,i,ans);
                 l.remove(l.size()-1);
             }
         }
