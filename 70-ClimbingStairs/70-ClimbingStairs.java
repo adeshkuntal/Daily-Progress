8e0293308eb01d00deaba6e08350fc3dc6eaa5c4
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2025-10-12 20:43:36 +0530

    File updated at 10/12/2025, 8:42:21 PM

--- 70-ClimbingStairs/70-ClimbingStairs.java
+++ 70-ClimbingStairs/70-ClimbingStairs.java
@@ -1,17 +1,13 @@
-// Last updated: 10/12/2025, 8:41:32 PM
+// Last updated: 10/12/2025, 8:42:21 PM
 class Solution {
     public int climbStairs(int n) {
-        if (n == 1) {
-            return 1;
-        }
+        if (n <= 2) return n;
         int[] dp = new int[n + 1];
         dp[1] = 1;
         dp[2] = 2;
-
         for (int i = 3; i <= n; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
-
         return dp[n];
     }
 }
