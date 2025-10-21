8b928fe6549140f99b1a80a2ec25229026549b0b
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2025-10-21 13:49:24 +0530

    Create 155-MinStack

---
+++ 155-MinStack/155-MinStack.java
@@ -0,0 +1,36 @@
+// Last updated: 10/21/2025, 1:48:10 PM
+import java.util.Stack;
+
+class MinStack {
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+    
+    public void pop() {
+        if (!stack.isEmpty()) {
+            int val = stack.pop();
+            if (val == minStack.peek()) {
+                minStack.pop();
+            }
+        }
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
