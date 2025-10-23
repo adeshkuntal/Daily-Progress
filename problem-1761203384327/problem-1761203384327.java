7c6f5949e0832ae22a5476a1d59037204f69513a
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2025-10-23 12:40:58 +0530

    Create

---
+++ problem-1761203384327/problem-1761203384327.java
@@ -0,0 +1,40 @@
+// Last updated: 10/23/2025, 12:39:44 PM
+import java.util.*;
+
+class Solution {
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<>();
+        int num = 0;
+        char prevOperator = '+';
+
+        for (int i = 0; i <= s.length(); i++) {
+            char ch = (i < s.length()) ? s.charAt(i) : '\0';
+
+            if (Character.isDigit(ch)) {
+                num = num * 10 + (ch - '0');
+            }
+
+            if ((!Character.isDigit(ch) && ch != ' ') || i == s.length()) {
+                if (prevOperator == '+') {
+                    stack.push(num);
+                } else if (prevOperator == '-') {
+                    stack.push(-num);
+                } else if (prevOperator == '*') {
+                    stack.push(stack.pop() * num);
+                } else if (prevOperator == '/') {
+                    stack.push(stack.pop() / num);
+                }
+
+                prevOperator = ch;
+                num = 0;
+            }
+        }
+
+        int result = 0;
+        while (!stack.isEmpty()) {
+            result += stack.pop();
+        }
+
+        return result;
+    }
+}
