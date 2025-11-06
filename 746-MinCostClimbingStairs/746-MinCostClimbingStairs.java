e27d7aa24a6f6c54e598e34294ac242fe85fa089
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2025-11-06 20:51:23 +0530

    File updated at 11/6/2025, 8:50:12 PM

--- 746-MinCostClimbingStairs/746-MinCostClimbingStairs.java
+++ 746-MinCostClimbingStairs/746-MinCostClimbingStairs.java
@@ -1,24 +1,55 @@
-// Last updated: 11/6/2025, 8:38:16 PM
+// Last updated: 11/6/2025, 8:50:12 PM
+// class Solution {
+//     public int minCostClimbingStairs(int[] cost) {
+//         int n = cost.length;
+//         int[] dp = new int[n+1];
+//         Arrays.fill(dp,-1);
+//         int ans = Math.min(findCost(cost,n-1,dp),findCost(cost,n-2,dp));
+//         return ans;
+//     }
+//     public static int findCost(int[] cost,int n,int[] dp){
+//         if(n == 0){
+//             return cost[0];
+//         }
+//         if(n == 1){
+//             return cost[1];
+//         }
+//         if(dp[n] != -1){
+//             return dp[n];
+//         }
+
+//         dp[n] = cost[n] + Math.min(findCost(cost,n-1,dp),findCost(cost,n-2,dp));
+//         return dp[n];
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
         int[] dp = new int[n+1];
         Arrays.fill(dp,-1);
-        int ans = Math.min(findCost(cost,n-1,dp),findCost(cost,n-2,dp));
-        return ans;
-    }
-    public static int findCost(int[] cost,int n,int[] dp){
-        if(n == 0){
-            return cost[0];
-        }
         if(n == 1){
             return cost[1];
         }
-        if(dp[n] != -1){
-            return dp[n];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for (int i = 2; i < n; i++) {
+            dp[i] = cost[i] + Math.min(dp[i - 1] , dp[i - 2]);
         }
-
-        dp[n] = cost[n] + Math.min(findCost(cost,n-1,dp),findCost(cost,n-2,dp));
-        return dp[n];
+        return Math.min(dp[n-1],dp[n-2]);
     }
+    
 }
