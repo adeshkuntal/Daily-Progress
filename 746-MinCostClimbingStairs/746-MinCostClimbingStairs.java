3212841f42fe523192ac55024d06cc1166b1cb47
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2025-11-06 21:03:11 +0530

    File updated at 11/6/2025, 9:02:00 PM

--- 746-MinCostClimbingStairs/746-MinCostClimbingStairs.java
+++ 746-MinCostClimbingStairs/746-MinCostClimbingStairs.java
@@ -1,4 +1,4 @@
-// Last updated: 11/6/2025, 8:50:12 PM
+// Last updated: 11/6/2025, 9:02:00 PM
 // class Solution {
 //     public int minCostClimbingStairs(int[] cost) {
 //         int n = cost.length;
@@ -30,6 +30,24 @@
 
 
 
+// class Solution {
+//     public int minCostClimbingStairs(int[] cost) {
+//         int n = cost.length;
+//         int[] dp = new int[n+1];
+//         Arrays.fill(dp,-1);
+//         if(n == 1){
+//             return cost[1];
+//         }
+//         dp[0] = cost[0];
+//         dp[1] = cost[1];
+//         for (int i = 2; i < n; i++) {
+//             dp[i] = cost[i] + Math.min(dp[i - 1] , dp[i - 2]);
+//         }
+//         return Math.min(dp[n-1],dp[n-2]);
+//     }
+    
+// }
+
 
 
 
@@ -39,17 +57,17 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
-        int[] dp = new int[n+1];
-        Arrays.fill(dp,-1);
         if(n == 1){
             return cost[1];
         }
-        dp[0] = cost[0];
-        dp[1] = cost[1];
+        int prev2 = cost[0];
+        int prev1 = cost[1];
         for (int i = 2; i < n; i++) {
-            dp[i] = cost[i] + Math.min(dp[i - 1] , dp[i - 2]);
+            int curr = cost[i] + Math.min(prev1 , prev2);
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return Math.min(dp[n-1],dp[n-2]);
+        return Math.min(prev1,prev2);
     }
     
 }
