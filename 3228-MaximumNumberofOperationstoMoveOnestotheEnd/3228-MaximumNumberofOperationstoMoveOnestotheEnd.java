5c7c6da82e05383cd7d7080edefe4273dff2f76b
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2025-11-13 21:18:13 +0530

    File updated at 11/13/2025, 9:17:04 PM

--- 3228-MaximumNumberofOperationstoMoveOnestotheEnd/3228-MaximumNumberofOperationstoMoveOnestotheEnd.java
+++ 3228-MaximumNumberofOperationstoMoveOnestotheEnd/3228-MaximumNumberofOperationstoMoveOnestotheEnd.java
@@ -1,10 +1,9 @@
-// Last updated: 11/13/2025, 9:16:36 PM
+// Last updated: 11/13/2025, 9:17:04 PM
 class Solution {
     public int maxOperations(String s) {
         int res = 0;
         int one = 0;
-        int n = s.length();
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < s.length(); ++i) {
             one += s.charAt(i) - '0';
             if (i > 0 && s.charAt(i) < s.charAt(i - 1)) {
                 res += one;
