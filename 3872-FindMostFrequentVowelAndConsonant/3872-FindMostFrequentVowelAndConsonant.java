ae24b5d6383b5221b96d005d969b5a3bdd80a8c1
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2025-11-22 13:18:14 +0530

    Create 3872-FindMostFrequentVowelAndConsonant

---
+++ 3872-FindMostFrequentVowelAndConsonant/3872-FindMostFrequentVowelAndConsonant.java
@@ -0,0 +1,25 @@
+// Last updated: 11/22/2025, 1:17:08 PM
+class Solution {
+    public int maxFreqSum(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int vowel = 0;
+        int cons = 0;
+        for (char ch : s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        for (char ch : s.toCharArray()) {
+            if(isVowel(ch)){
+                vowel = Math.max(vowel,map.get(ch));
+            }
+            else{
+                cons = Math.max(cons,map.get(ch));
+            }
+        }
+
+        return vowel+cons;
+    }
+
+    private static boolean isVowel(char ch) {
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+    }
+}
