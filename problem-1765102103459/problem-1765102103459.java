e8b6e330e3acb5631c5756f2fe387f28a6910b71
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2025-12-07 15:39:24 +0530

    Create

---
+++ problem-1765102103459/problem-1765102103459.java
@@ -0,0 +1,10 @@
+// Last updated: 12/7/2025, 3:38:23 PM
+1class Solution {
+2    public int countOdds(int low, int high) {
+3        if (low % 2 == 0 && high % 2 == 0) {
+4            return (high - low) / 2;
+5        } else {
+6            return (high - low) / 2 + 1;
+7        }
+8    }
+9}
