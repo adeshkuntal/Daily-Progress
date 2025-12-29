69ff1e24ebf20d8ac00f871076eae279a42f5cae
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2025-12-29 20:23:00 +0530

    File updated at 12/29/2025, 8:22:07 PM

--- 344-ReverseString/344-ReverseString.java
+++ 344-ReverseString/344-ReverseString.java
@@ -1,18 +1,14 @@
-// Last updated: 12/29/2025, 8:21:24 PM
+// Last updated: 12/29/2025, 8:22:07 PM
 1class Solution {
 2    public void reverseString(char[] s) {
-3        int left = 0;
-4        int right = s.length - 1;
-5        
-6        while (left < right) {
-7            // Swap characters
-8            char temp = s[left];
-9            s[left] = s[right];
-10            s[right] = temp;
-11            
-12            // Move towards the middle
-13            left++;
-14            right--;
-15        }
-16    }
-17}
+3        int l = 0;
+4        int r = s.length - 1;
+5        while(l < r){
+6            char c = s[l];
+7            s[l] = s[r];
+8            s[r] = c;
+9            l++;
+10            r--;
+11        }
+12    }
+13}
