a318ff8c62f34637a72e46f9e4e0b37782a02932
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2025-12-29 20:28:48 +0530

    File updated at 12/29/2025, 8:27:55 PM

--- 509-FibonacciNumber/509-FibonacciNumber.java
+++ 509-FibonacciNumber/509-FibonacciNumber.java
@@ -1,18 +1,13 @@
-// Last updated: 11/5/2025, 7:31:44 PM
-class Solution {
-    public int fib(int n) {
-        if (n == 0){
-            return 0;
-        }
-        if (n == 1){
-            return 1;
-        }
-        int[] dp = new int[n + 1];
-        dp[0] = 0;
-        dp[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
-        }
-        return dp[n];
-    }
-}
+// Last updated: 12/29/2025, 8:27:55 PM
+1class Solution {
+2    public int fib(int n) {
+3        if(n == 0){
+4            return 0;
+5        }
+6        if(n == 1){
+7            return 1;
+8        }
+9
+10        return fib(n-2)+fib(n-1);
+11    }
+12}
