ece856b77224a45d8f5740ba377dedc14ccfc58a
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2025-12-30 11:45:14 +0530

    File updated at 12/30/2025, 11:44:22 AM

--- 200-NumberofIslands/200-NumberofIslands.java
+++ 200-NumberofIslands/200-NumberofIslands.java
@@ -1,32 +1,32 @@
-// Last updated: 9/29/2025, 10:21:32 PM
-class Solution {
-    public int numIslands(char[][] grid) {
-        int islands = 0;
-        int m = grid.length;
-        int n = grid[0].length;
-        boolean[][] vis = new boolean[m][n];
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '1' && !vis[i][j]) {
-                    dfs(i, j, vis, grid, m, n);
-                    islands++;
-                }
-            }
-        }
-        return islands;
-    }
-
-    void dfs(int i, int j, boolean[][] vis, char[][] grid, int m, int n) {
-        if (i < 0 || j < 0 || i >= m || j >= n || vis[i][j] || grid[i][j] != '1') {
-            return;
-        }
-
-        vis[i][j] = true;
-
-        dfs(i - 1, j, vis, grid, m, n); // top
-        dfs(i, j + 1, vis, grid, m, n); // right
-        dfs(i + 1, j, vis, grid, m, n); // bottom
-        dfs(i, j - 1, vis, grid, m, n); // left
-    }
-}
+// Last updated: 12/30/2025, 11:44:22 AM
+1class Solution {
+2    public int numIslands(char[][] grid) {
+3        int islands = 0;
+4        int m = grid.length;
+5        int n = grid[0].length;
+6        boolean[][] vis = new boolean[m][n];
+7
+8        for (int i = 0; i < m; i++) {
+9            for (int j = 0; j < n; j++) {
+10                if (grid[i][j] == '1' && !vis[i][j]) {
+11                    dfs(i, j, vis, grid, m, n);
+12                    islands++;
+13                }
+14            }
+15        }
+16        return islands;
+17    }
+18
+19    void dfs(int i, int j, boolean[][] vis, char[][] grid, int m, int n) {
+20        if (i < 0 || j < 0 || i >= m || j >= n || vis[i][j] || grid[i][j] != '1') {
+21            return;
+22        }
+23
+24        vis[i][j] = true;
+25
+26        dfs(i - 1, j, vis, grid, m, n); // top
+27        dfs(i, j + 1, vis, grid, m, n); // right
+28        dfs(i + 1, j, vis, grid, m, n); // bottom
+29        dfs(i, j - 1, vis, grid, m, n); // left
+30    }
+31}
