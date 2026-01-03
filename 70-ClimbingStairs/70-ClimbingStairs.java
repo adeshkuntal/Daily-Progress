ea999c48b062b3dbb8d16ca6d1acaa122703d16b
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-01-03 12:49:32 +0530

    File updated at 1/3/2026, 12:48:41 PM

--- 70-ClimbingStairs/70-ClimbingStairs.java
+++ 70-ClimbingStairs/70-ClimbingStairs.java
@@ -1,13 +1,22 @@
-// Last updated: 10/12/2025, 8:42:21 PM
-class Solution {
-    public int climbStairs(int n) {
-        if (n <= 2) return n;
-        int[] dp = new int[n + 1];
-        dp[1] = 1;
-        dp[2] = 2;
-        for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
-        }
-        return dp[n];
-    }
-}
+// Last updated: 1/3/2026, 12:48:41 PM
+1class Solution {
+2    public int rob(int[] nums) {
+3        int[] dp = new int[nums.length];
+4        Arrays.fill(dp, -1);
+5        return Robber(nums, 0, dp);
+6    }
+7    public int Robber(int[] nums, int i, int[] dp) {
+8        if(i >= nums.length) {
+9            return 0;
+10        }
+11        if(dp[i] != -1) {
+12            return dp[i];
+13        }
+14        int rob = nums[i] + Robber(nums,i+2,dp);
+15        int donotRob = Robber(nums,i+1,dp);
+16
+17        dp[i] = Math.max(rob,donotRob);
+18
+19        return dp[i];
+20    }
+21}
