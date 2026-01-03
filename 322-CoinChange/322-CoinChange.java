f9cfcbb6cc3a4cf6aa13ca571c4d4171c89678e0
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-01-03 15:41:52 +0530

    File updated at 1/3/2026, 3:41:01 PM

--- 322-CoinChange/322-CoinChange.java
+++ 322-CoinChange/322-CoinChange.java
@@ -1,60 +1,59 @@
-// Last updated: 11/8/2025, 2:51:30 PM
-// import java.util.*;
-
-// class Solution {
-//     public int coinChange(int[] coins, int amount) {
-//         int[] dp = new int[amount + 1];
-//         Arrays.fill(dp, -1);
-
-//         int res = find_coin(coins, amount, dp);
-//         return res == Integer.MAX_VALUE ? -1 : res;
-//     }
-
-//     public static int find_coin(int[] coins, int amount, int[] dp) {
-//         if (amount == 0) {
-//             return 0;
-//         }
-//         if (amount < 0) {
-//             return Integer.MAX_VALUE;
-//         }
-//         if (dp[amount] != -1) {
-//             return dp[amount];
-//         }
-
-//         int min = Integer.MAX_VALUE;
-//         for (int i = 0; i < coins.length; i++) {
-//             int res = find_coin(coins, amount - coins[i], dp);
-//             if (res != Integer.MAX_VALUE) {
-//                 min = Math.min(min, res + 1);
-//             }
-//         }
-//         dp[amount] = min;
-//         return min;
-//     }
-// }
-
-
-
-//Tabulation
-class Solution {
-    public int coinChange(int[] coins, int amount) {
-        int[] dp = new int[amount + 1];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-
-        if (amount == 0) {
-            return 0;
-        }
-
-        dp[0] = 0;
-        for (int i = 1; i <= amount; i++) {
-            for (int j = 0; j < coins.length; j++) {
-                if (i - coins[j] >= 0 && dp[i - coins[j]] != Integer.MAX_VALUE) {
-                    dp[i] = Math.min(dp[i], 1 + dp[i - coins[j]]);
-                }
-            }
-        }
-
-        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
-    }
-}
-
+// Last updated: 1/3/2026, 3:41:01 PM
+1class Solution {
+2    public int coinChange(int[] coins, int amount) {
+3        int[] dp = new int[amount + 1];
+4        Arrays.fill(dp, -1);
+5
+6        int res = find_coin(coins, amount, dp);
+7        return res == Integer.MAX_VALUE ? -1 : res;
+8    }
+9
+10    public static int find_coin(int[] coins, int amount, int[] dp) {
+11        if (amount == 0) {
+12            return 0;
+13        }
+14        if (amount < 0) {
+15            return Integer.MAX_VALUE;
+16        }
+17        if (dp[amount] != -1) {
+18            return dp[amount];
+19        }
+20
+21        int min = Integer.MAX_VALUE;
+22        for (int i = 0; i < coins.length; i++) {
+23            int res = find_coin(coins, amount - coins[i], dp);
+24            if (res != Integer.MAX_VALUE) {
+25                min = Math.min(min, res + 1);
+26            }
+27        }
+28        dp[amount] = min;
+29        return min;
+30    }
+31}
+32
+33
+34
+35//Tabulation
+36// class Solution {
+37//     public int coinChange(int[] coins, int amount) {
+38//         int[] dp = new int[amount + 1];
+39//         Arrays.fill(dp, Integer.MAX_VALUE);
+40
+41//         if (amount == 0) {
+42//             return 0;
+43//         }
+44
+45//         dp[0] = 0;
+46//         for (int i = 1; i <= amount; i++) {
+47//             for (int j = 0; j < coins.length; j++) {
+48//                 if (i - coins[j] >= 0 && dp[i - coins[j]] != Integer.MAX_VALUE) {
+49//                     dp[i] = Math.min(dp[i], 1 + dp[i - coins[j]]);
+50//                 }
+51//             }
+52//         }
+53
+54//         return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+55//     }
+56// }
+57
+58
