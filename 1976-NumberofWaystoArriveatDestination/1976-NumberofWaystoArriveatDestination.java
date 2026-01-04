5329d9fc3c761e56ff884704f86db62ae7e5e0c0
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-01-04 19:56:21 +0530

    File updated at 1/4/2026, 7:55:31 PM

--- 1976-NumberofWaystoArriveatDestination/1976-NumberofWaystoArriveatDestination.java
+++ 1976-NumberofWaystoArriveatDestination/1976-NumberofWaystoArriveatDestination.java
@@ -1,4 +1,4 @@
-// Last updated: 1/4/2026, 7:54:02 PM
+// Last updated: 1/4/2026, 7:55:31 PM
 1class Solution {
 2    public int countPaths(int n, int[][] roads) {
 3        int mod = 1000000007;
@@ -29,23 +29,25 @@
 28            int node = (int) curr[0];
 29            long time = curr[1];
 30
-31            if(time > dist[node]) continue;
-32
-33            for(int[] x : map.get(node)){
-34                int next = x[0];
-35                long newTime = time + x[1];
-36
-37                if(newTime < dist[next]){
-38                    dist[next] = newTime;
-39                    ways[next] = ways[node];
-40                    pq.add(new long[]{next, newTime});
-41                } else if(newTime == dist[next]){
-42                    ways[next] = (ways[next] + ways[node]) % mod;
-43                }
-44            }
-45        }
-46
-47        return ways[n - 1];
-48    }
-49}
-50
+31            if(time > dist[node]){
+32                continue;
+33            }
+34
+35            for(int[] x : map.get(node)){
+36                int next = x[0];
+37                long newTime = time + x[1];
+38
+39                if(newTime < dist[next]){
+40                    dist[next] = newTime;
+41                    ways[next] = ways[node];
+42                    pq.add(new long[]{next, newTime});
+43                } else if(newTime == dist[next]){
+44                    ways[next] = (ways[next] + ways[node]) % mod;
+45                }
+46            }
+47        }
+48
+49        return ways[n - 1];
+50    }
+51}
+52
