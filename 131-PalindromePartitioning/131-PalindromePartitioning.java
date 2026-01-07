47e5441ccf2e16cc788a617c8d7f6fed63d167d1
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-01-07 16:32:01 +0530

    File updated at 1/7/2026, 4:31:12 PM

--- 131-PalindromePartitioning/131-PalindromePartitioning.java
+++ 131-PalindromePartitioning/131-PalindromePartitioning.java
@@ -1,37 +1,35 @@
-// Last updated: 10/6/2025, 5:11:39 PM
-class Solution {
-    public List<List<String>> partition(String s) {
-        List<List<String>> res = new ArrayList<>();
-        List<String> l = new ArrayList<>();
-        find_partition(s,res,l);
-        return res;
-    }
-    public static void find_partition(String s, List<List<String>> res, List<String> l){
-        if(s.length() == 0){
-            res.add(new ArrayList<>(l));
-            return;
-        }
-
-        for(int i=1; i<=s.length(); i++){
-            String x = s.substring(0,i);
-            if(ispalindrome(x)){
-                l.add(x);
-                find_partition(s.substring(i),res,l);
-                l.remove(l.size()-1);
-            }
-        }
-    }
-    public static boolean ispalindrome(String x){
-        int left = 0;
-        int right = x.length() - 1;
-        while(left <= right){
-            if(x.charAt(left) != x.charAt(right)){
-                return false;
-            }
-            left++;
-            right--;
-        }
-
-        return true;
-    }
-}
+// Last updated: 1/7/2026, 4:31:12 PM
+1class Solution {
+2    public List<List<String>> partition(String s) {
+3        List<List<String>> res = new ArrayList<>();
+4        List<String> l = new ArrayList<>();
+5        find_partition(res,l,s);
+6        return res;
+7    }
+8    public void find_partition(List<List<String>> res,List<String> ans,String ques){
+9        if(ques.length() == 0){
+10            res.add(new ArrayList<>(ans));
+11            return;
+12        }
+13        for(int i=1; i<=ques.length(); i++){
+14            String s = ques.substring(0,i);
+15            if(isPalindrome(s)){
+16                ans.add(s);
+17                find_partition(res,ans,ques.substring(i));
+18                ans.remove(ans.size() - 1);
+19            }
+20        }
+21    }
+22    public boolean isPalindrome(String s){
+23        int i = 0;
+24        int j = s.length() - 1;
+25        while(i < j){
+26            if(s.charAt(i) != s.charAt(j)){
+27                return false;
+28            }
+29            i++;
+30            j--;
+31        }
+32        return true;
+33    }
+34}
