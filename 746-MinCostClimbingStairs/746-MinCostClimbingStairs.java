7dd9ca118a739820f2afeadafdc61cdc9008effb
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-01-09 21:10:26 +0530

    File updated at 1/9/2026, 9:09:39 PM

--- 746-MinCostClimbingStairs/746-MinCostClimbingStairs.java
+++ 746-MinCostClimbingStairs/746-MinCostClimbingStairs.java
@@ -1,71 +1,21 @@
-// Last updated: 1/9/2026, 8:01:58 PM
+// Last updated: 1/9/2026, 9:09:39 PM
 1class Solution {
-2    public int minCostClimbingStairs(int[] cost) {
-3        int n = cost.length;
-4        int[] dp = new int[n+1];
-5        Arrays.fill(dp,-1);
-6        int ans = Math.min(findCost(cost,0,dp),findCost(cost,1,dp));
-7        return ans;
-8    }
-9    public static int findCost(int[] cost,int n,int[] dp){
-10        if(n >= cost.length){
-11            return 0;
-12        }
-13        if(dp[n] != -1){
-14            return dp[n];
-15        }
-16        int one_step = findCost(cost,n+1,dp);
-17        int two_step = findCost(cost,n+2,dp);
-18        dp[n] = cost[n] + Math.min(one_step,two_step);
-19        return dp[n];
-20    }
-21}
-22
-23
-24
-25
-26
-27
-28
-29
-30// class Solution {
-31//     public int minCostClimbingStairs(int[] cost) {
-32//         int n = cost.length;
-33//         int[] dp = new int[n+1];
-34//         Arrays.fill(dp,-1);
-35//         if(n == 1){
-36//             return cost[1];
-37//         }
-38//         dp[0] = cost[0];
-39//         dp[1] = cost[1];
-40//         for (int i = 2; i < n; i++) {
-41//             dp[i] = cost[i] + Math.min(dp[i - 1] , dp[i - 2]);
-42//         }
-43//         return Math.min(dp[n-1],dp[n-2]);
-44//     }
-45    
-46// }
-47
-48
-49
-50
-51
-52
-53
-54// class Solution {
-55//     public int minCostClimbingStairs(int[] cost) {
-56//         int n = cost.length;
-57//         if(n == 1){
-58//             return cost[1];
-59//         }
-60//         int prev2 = cost[0];
-61//         int prev1 = cost[1];
-62//         for (int i = 2; i < n; i++) {
-63//             int curr = cost[i] + Math.min(prev1 , prev2);
-64//             prev2 = prev1;
-65//             prev1 = curr;
-66//         }
-67//         return Math.min(prev1,prev2);
-68//     }
-69    
-70// }
+2    public int lengthOfLIS(int[] arr) {
+3        int[] lis = new int[arr.length];
+4        Arrays.fill(lis,1);
+5        for(int i=1; i<arr.length; i++){
+6            for(int j=i-1; j>=0; j--){
+7                if(arr[j] < arr[i]){
+8                    lis[i] = Math.max(lis[i] , lis[j]+1);
+9                }
+10            }
+11        }
+12
+13        int max = lis[0];
+14        for(int i=1; i<lis.length; i++){
+15            max = Math.max(max,lis[i]);
+16        }
+17
+18        return max;
+19    }
+20}
