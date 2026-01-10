f4e7ecf96ae554d2544ca5bcd0b6d2735e645f02
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-01-10 16:15:03 +0530

    File updated at 1/10/2026, 4:14:15 PM

--- 1035-UncrossedLines/1035-UncrossedLines.java
+++ 1035-UncrossedLines/1035-UncrossedLines.java
@@ -1,28 +1,29 @@
-// Last updated: 1/10/2026, 3:38:12 PM
+// Last updated: 1/10/2026, 4:14:15 PM
 1class Solution {
-2    public int maxUncrossedLines(int[] nums1, int[] nums2) {
-3        int[][] dp = new int[nums1.length][nums2.length];
+2    public int change(int amount, int[] coin) {
+3        int[][] dp = new int[amount+1][coin.length];
 4        for(int[] a : dp){
 5            Arrays.fill(a,-1);
 6        }
-7        return LCS(nums1,nums2,0,0,dp);
+7        return Coin(coin,amount,0,dp);
 8    }
-9    public static int LCS(int[] nums1, int[] nums2,int i,int j,int[][] dp){
-10        if(i==nums1.length || j==nums2.length){
-11            return 0;
+9    public static int Coin(int[] coin,int amount,int idx,int[][] dp){
+10        if(amount == 0){
+11            return 1;
 12        }
-13        if(dp[i][j] != -1){
-14            return dp[i][j];
+13        if(idx == coin.length){
+14            return 0;
 15        }
-16        int ans = 0;
-17        if(nums1[i] == nums2[j]){
-18            ans = 1 + LCS(nums1,nums2,i+1,j+1,dp);
-19        }
-20        else{
-21            int f = LCS(nums1,nums2,i+1,j,dp);
-22            int s = LCS(nums1,nums2,i,j+1,dp);
-23            ans = Math.max(f,s);
-24        }
-25        return dp[i][j] = ans;
-26    }
-27}
+16        int inc = 0;
+17        int exc = 0;
+18        if(dp[amount][idx] != -1){
+19            return dp[amount][idx];
+20        }
+21        if(amount >= coin[idx]){
+22            inc = Coin(coin,amount-coin[idx],idx,dp);
+23        }
+24        exc = Coin(coin,amount,idx+1,dp);
+25
+26        return dp[amount][idx] = inc + exc;
+27    }
+28}
