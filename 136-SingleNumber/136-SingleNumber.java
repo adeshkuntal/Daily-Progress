6bae405ff27f69b04d4e72873c27fe46148bbafd
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-01-15 20:56:56 +0530

    File updated at 1/15/2026, 8:56:10 PM

--- 136-SingleNumber/136-SingleNumber.java
+++ 136-SingleNumber/136-SingleNumber.java
@@ -1,19 +1,10 @@
-// Last updated: 8/4/2025, 6:57:20 PM
-class Solution {
-    public int singleNumber(int[] nums) {
-        int c =0;
-        int a = 0;
-        for(int i=0;i<nums.length;i++){
-            for(int j=0;j<nums.length;j++){
-                if(i!=j && nums[i]==nums[j]){
-                    c++;
-                }
-            }
-            if(c==0){
-                a=i;
-            }
-            c=0;
-        }
-        return nums[a];
-    }
-}
+// Last updated: 1/15/2026, 8:56:10 PM
+1class Solution {
+2    public int singleNumber(int[] nums) {
+3        int ans = 0;
+4        for(int i=0; i<nums.length; i++){
+5            ans = ans^nums[i];
+6        }
+7        return ans;
+8    }
+9}
