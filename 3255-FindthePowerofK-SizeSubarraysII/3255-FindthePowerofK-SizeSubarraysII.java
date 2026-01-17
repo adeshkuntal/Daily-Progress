6b8808572e4fab0daa7770d52481dfc754ff815d
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-01-17 15:56:58 +0530

    File updated at 1/17/2026, 3:56:12 PM

--- 3255-FindthePowerofK-SizeSubarraysII/3255-FindthePowerofK-SizeSubarraysII.java
+++ 3255-FindthePowerofK-SizeSubarraysII/3255-FindthePowerofK-SizeSubarraysII.java
@@ -1,27 +1,59 @@
-// Last updated: 1/17/2026, 3:55:57 PM
-1class Solution {
-2    public int[] resultsArray(int[] nums, int k) {
-3        int n=nums.length;
-4        int res[]=new int[n-k+1];
-5        int cons=1;
-6        if(k==1){
-7             return nums;
-8        }
-9        for (int i=1;i<n;i++){
-10            if(nums[i]==nums[i-1]+1){
-11                cons++;
-12            }
-13            else{
-14                cons=1;
-15            }
-16            if(i>=k-1){
-17                if(cons>=k){
-18                    res[i-k+1]=nums[i];
-19                }
-20                else{
-21                    res[i-k+1]=-1;
-22                }
-23            }}
-24        return res;
-25    }
-26}
+// Last updated: 1/17/2026, 3:56:12 PM
+1// class Solution {
+2//     public int[] resultsArray(int[] nums, int k) {
+3//         int n = nums.length;
+4//         int[] res = new int[n-k+1];
+5//         for(int i=0; i<res.length; i++){
+6//             if(isPower(nums,i,i+k-1)){
+7//                 res[i] = nums[i+k-1];
+8//             }
+9//             else{
+10//                 res[i] = -1;
+11//             }
+12//         }
+13//         return res;
+14//     }
+15
+16//     public static boolean isPower(int[] nums,int si,int ei){
+17//         int x = nums[si++];
+18//         while(si <= ei){
+19//             if(nums[si] != x + 1){
+20//                 return false;
+21//             }
+22//             x = nums[si];
+23//             si++;
+24//         }
+25//         return true;
+26//     }
+27// }
+28
+29
+30
+31
+32class Solution {
+33    public int[] resultsArray(int[] nums, int k) {
+34        int n=nums.length;
+35        int res[]=new int[n-k+1];
+36        int cons=1;
+37        if(k==1){
+38             return nums;
+39        }
+40        for (int i=1;i<n;i++){
+41            if(nums[i]==nums[i-1]+1){
+42                cons++;
+43            }
+44            else{
+45                cons=1;
+46            }
+47            if(i>=k-1){
+48                if(cons>=k){
+49                    res[i-k+1]=nums[i];
+50                }
+51                else{
+52                    res[i-k+1]=-1;
+53                }
+54            }}
+55        return res;
+56    }
+57}
+58
