b4f97c13a009ee6160f8bfd238eb4e6d0111075a
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-01-19 21:45:36 +0530

    File updated at 1/19/2026, 9:44:51 PM

--- 907-SumofSubarrayMinimums/907-SumofSubarrayMinimums.java
+++ 907-SumofSubarrayMinimums/907-SumofSubarrayMinimums.java
@@ -1,69 +1,71 @@
-// Last updated: 1/19/2026, 12:35:12 PM
+// Last updated: 1/19/2026, 9:44:51 PM
 1// class Solution {
 2//     public int sumSubarrayMins(int[] arr) {
-3//         long mod = 1000000007;
-4//         long sum = 0;
+3//         int mod = (int) 1e9 + 7;
+4//         int sum = 0;
 5//         for(int i=0; i<arr.length; i++){
-6//             for(int j=i; j<arr.length; j++){
-7//                 sum += findMin(i,j,arr);
-8//             }
-9        
-10
-11//         return (int) (sum % mod);
-12//     }
-13//     public int findMin(int si,int ei,int[] arr){
-14//         int min = arr[si];
-15//         for(int i=si+1; i<=ei; i++){
-16//             if(arr[i] < min){
-17//                 min = arr[i];
-18//             }
-19//         }
+6//             int min = arr[i];
+7//             for(int j=i; j<arr.length; j++){
+8//                 min = Math.min(min,arr[j]);
+9//                 sum = (sum + min) % mod;
+10//             }
+11//         }
+12
+13//         return sum;
+14//     }
+15// }
+16
+17
+18
+19
 20
-21//         return min;
-22//     }
-23// }
-24
+21class Solution {
+22    public int sumSubarrayMins(int[] arr) {
+23        int n = arr.length;
+24        int mod = 1000000007;
 25
-26
-27
+26        int[] left = new int[n];
+27        int[] right = new int[n];
 28
-29
-30class Solution {
-31    public int sumSubarrayMins(int[] arr) {
-32        int n = arr.length;
-33        int mod = 1000000007;
-34        int[] left = new int[n];
-35        int[] right = new int[n];
-36        Stack<Integer> st = new Stack<>();
-37
-38        for(int i = 0; i < n; i++){
-39            while(!st.isEmpty() && arr[st.peek()] > arr[i]){
-40                st.pop();
-41            }
-42            left[i] = st.isEmpty() ? i + 1 : i - st.peek();
-43            st.push(i);
-44        }
-45
-46        st.clear();
-47
-48        for(int i = n - 1; i >= 0; i--){
-49            while(!st.isEmpty() && arr[st.peek()] >= arr[i]){
-50                st.pop();
-51            }
-52            right[i] = st.isEmpty() ? n - i : st.peek() - i;
-53            st.push(i);
-54        }
-55
-56        long sum = 0;
-57        for(int i = 0; i < n; i++){
-58            sum = (sum + (long)arr[i] * left[i] * right[i]) % mod;
-59        }
-60
-61        return (int) sum;
-62    }
-63}
-64
-65
-66
+29        Stack<Integer> st = new Stack<>();
+30
+31        for(int i = 0; i < n; i++){
+32            if(st.isEmpty()){
+33                left[i] = -1;
+34            } else {
+35                while(!st.isEmpty() && arr[st.peek()] > arr[i]){
+36                    st.pop();
+37                }
+38                left[i] = st.isEmpty() ? -1 : st.peek();
+39            }
+40            st.push(i);
+41        }
+42
+43        st.clear();
+44
+45        for(int i = n - 1; i >= 0; i--){
+46            if(st.isEmpty()){
+47                right[i] = n;
+48            } else {
+49                while(!st.isEmpty() && arr[st.peek()] >= arr[i]){
+50                    st.pop();
+51                }
+52                right[i] = st.isEmpty() ? n : st.peek();
+53            }
+54            st.push(i);
+55        }
+56
+57        long sum = 0;
+58        for(int i = 0; i < n; i++){
+59            long l = i - left[i];
+60            long r = right[i] - i;
+61            sum = (sum + arr[i] * l * r) % mod;
+62        }
+63
+64        return (int) sum;
+65    }
+66}
 67
 68
+69
+70
