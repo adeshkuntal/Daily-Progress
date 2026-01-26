48913ab7c39a5986281a6bb453b94a005cc58c84
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-01-26 21:52:19 +0530

    File updated at 1/26/2026, 9:51:38 PM

--- 2332-TheLatestTimetoCatchaBus/2332-TheLatestTimetoCatchaBus.java
+++ 2332-TheLatestTimetoCatchaBus/2332-TheLatestTimetoCatchaBus.java
@@ -1,23 +1,25 @@
-// Last updated: 1/26/2026, 9:51:23 PM
+// Last updated: 1/26/2026, 9:51:38 PM
 1class Solution {
 2    public int latestTimeCatchTheBus(int[] buses, int[] passengers, int capacity) {
 3        Arrays.sort(buses);
 4        Arrays.sort(passengers);
-5        Set<Integer> set = new HashSet<>();
-6        int ans=0,j=0;
-7        for(int i=0;i<buses.length;i++){
-8            int c = 0;
-9            while(j<passengers.length && c<capacity && passengers[j]<=buses[i]){
-10                if(!set.contains(passengers[j]-1)){
-11                    ans=passengers[j]-1;
-12                }
-13                set.add(passengers[j]);
-14                j++; c++;
-15            }
-16            if(c<capacity && !set.contains(buses[i])){
-17                ans = buses[i];
-18            }
-19        }
-20        return ans;
-21    }
-22}
+5
+6        Set<Integer> set = new HashSet<>();
+7        int ans=0;
+8        int j=0;
+9        for(int i=0;i<buses.length;i++){
+10            int c = 0;
+11            while(j<passengers.length && c<capacity && passengers[j]<=buses[i]){
+12                if(!set.contains(passengers[j]-1)){
+13                    ans=passengers[j]-1;
+14                }
+15                set.add(passengers[j]);
+16                j++; c++;
+17            }
+18            if(c<capacity && !set.contains(buses[i])){
+19                ans = buses[i];
+20            }
+21        }
+22        return ans;
+23    }
+24}
