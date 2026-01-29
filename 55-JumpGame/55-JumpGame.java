867c39d2752ce32b76e6b3e91f497596eb866dcc
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-01-29 15:38:29 +0530

    File updated at 1/29/2026, 3:37:48 PM

--- 55-JumpGame/55-JumpGame.java
+++ 55-JumpGame/55-JumpGame.java
@@ -1,18 +1,22 @@
-// Last updated: 1/29/2026, 2:12:18 PM
+// Last updated: 1/29/2026, 3:37:48 PM
 1class Solution {
-2    public boolean canJump(int[] nums) {
+2    public int jump(int[] nums) {
 3        int pos = 0;
 4        int i = 0;
-5
-6        while (i <= pos) {
-7            if (pos >= nums.length - 1) {
-8                return true;
-9            }
-10            pos = Math.max(pos, i + nums[i]);
-11            i++;
-12        }
-13
-14        return false;
-15    }
-16}
+5        int count = 0;
+6        int end = 0;
+7
+8        while (i < nums.length - 1) {
+9            pos = Math.max(pos, i + nums[i]);
+10
+11            if (i == end) {
+12                count++;
+13                end = pos;
+14            }
+15            i++;
+16        }
 17
+18        return count;
+19    }
+20}
+21
