3dc60a0f01a7a34b4e6700e4ca6a6aa388e20bff
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-01-29 17:10:02 +0530

    File updated at 1/29/2026, 5:09:22 PM

--- 55-JumpGame/55-JumpGame.java
+++ 55-JumpGame/55-JumpGame.java
@@ -1,22 +1,22 @@
-// Last updated: 1/29/2026, 3:37:48 PM
+// Last updated: 1/29/2026, 5:09:22 PM
 1class Solution {
 2    public int jump(int[] nums) {
-3        int pos = 0;
-4        int i = 0;
-5        int count = 0;
-6        int end = 0;
-7
-8        while (i < nums.length - 1) {
-9            pos = Math.max(pos, i + nums[i]);
-10
-11            if (i == end) {
-12                count++;
-13                end = pos;
-14            }
-15            i++;
-16        }
-17
-18        return count;
-19    }
-20}
+3        int i = 0;
+4        int pos = 0;
+5        int end = 0;
+6        int jump = 0;
+7        while(i < nums.length-1){
+8            pos = Math.max(pos,i+nums[i]);
+9            if(i==end){
+10                jump++;
+11                end = pos;
+12            }
+13            i++;
+14        }
+15
+16        return jump;
+17    }
+18}
+19
+20
 21
