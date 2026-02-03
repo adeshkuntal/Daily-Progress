57604cf99d209def45e47401afb20ec260fbf237
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-02-03 14:57:46 +0530

    File updated at 2/3/2026, 2:57:07 PM

--- 2289-StepstoMakeArrayNon-decreasing/2289-StepstoMakeArrayNon-decreasing.java
+++ 2289-StepstoMakeArrayNon-decreasing/2289-StepstoMakeArrayNon-decreasing.java
@@ -1,28 +1,22 @@
-// Last updated: 2/3/2026, 12:39:06 PM
+// Last updated: 2/3/2026, 2:57:07 PM
 1class Solution {
 2    public int totalSteps(int[] nums) {
-3        Stack<int[]> st = new Stack<>();
-4        st.push(new int[]{nums[0], 0});
+3        Stack<int[]> stack = new Stack<>();
+4        int max = 0;
 5
-6        int max = 0;
-7
-8        for(int i = 1; i < nums.length; i++){
-9            int count = 0;
-10
-11            while(!st.isEmpty() && nums[i] >= st.peek()[0]){
-12                count = Math.max(count, st.peek()[1]);
-13                st.pop();
-14            }
-15
-16            if(!st.isEmpty()){
-17                count++;
-18            }
-19
-20            max = Math.max(max, count);
-21            st.push(new int[]{nums[i], count});
-22        }
-23
-24        return max;
-25    }
-26}
-27
+6        for (int num : nums) {
+7            int score = 0;
+8
+9            while (!stack.isEmpty() && stack.peek()[0] <= num) {
+10                score = Math.max(score, stack.peek()[1]);
+11                stack.pop();
+12            }
+13
+14            int curr = stack.isEmpty() ? 0 : score + 1;
+15            stack.push(new int[]{num, curr});
+16            max = Math.max(max, curr);
+17        }
+18
+19        return max;
+20    }
+21}
