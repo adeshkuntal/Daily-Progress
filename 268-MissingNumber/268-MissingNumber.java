29315d4b92866f7de2f8450ce2f038274760e244
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-02-05 22:11:16 +0530

    File updated at 2/5/2026, 10:10:38 PM

--- 268-MissingNumber/268-MissingNumber.java
+++ 268-MissingNumber/268-MissingNumber.java
@@ -1,27 +1,30 @@
-// Last updated: 2/5/2026, 10:03:04 PM
-1class Solution {
-2    public int missingNumber(int[] nums) {
-3        int i = 0;
-4
-5        while (i < nums.length) {
-6            int correct = nums[i];
-7
-8            if (nums[i] < nums.length && nums[i] != nums[correct]) {
-9                int temp = nums[i];
-10                nums[i] = nums[correct];
-11                nums[correct] = temp;
-12            } else {
-13                i++;
-14            }
-15        }
-16
-17        for (int j = 0; j < nums.length; j++) {
-18            if (nums[j] != j) {
-19                return j;
-20            }
-21        }
-22
-23        return nums.length;
-24    }
-25}
-26
+// Last updated: 2/5/2026, 10:10:38 PM
+1import java.util.*;
+2
+3class Solution {
+4    public List<Integer> findDisappearedNumbers(int[] nums) {
+5        List<Integer> ans = new ArrayList<>();
+6
+7        int i = 0;
+8        while (i < nums.length) {
+9            int correct = nums[i] - 1;
+10
+11            if (nums[i] != nums[correct]) {
+12                int temp = nums[i];
+13                nums[i] = nums[correct];
+14                nums[correct] = temp;
+15            } else {
+16                i++;
+17            }
+18        }
+19
+20        for (int j = 0; j < nums.length; j++) {
+21            if (nums[j] != j + 1) {
+22                ans.add(j + 1);
+23            }
+24        }
+25
+26        return ans;
+27    }
+28}
+29
