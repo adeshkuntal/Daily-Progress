cc164d38054657b68de97aff13e6e445833caea7
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-02-05 12:09:53 +0530

    File updated at 2/5/2026, 12:09:16 PM

--- 648-ReplaceWords/648-ReplaceWords.java
+++ 648-ReplaceWords/648-ReplaceWords.java
@@ -1,24 +1,24 @@
-// Last updated: 2/5/2026, 12:08:07 PM
+// Last updated: 2/5/2026, 12:09:16 PM
 1class Solution {
 2    public String replaceWords(List<String> dictionary, String sentence) {
 3        String s = "";
 4        String[] words = sentence.split(" ");
-5        for (int i = 0; i < words.length; i++) {
-6            String replacement = words[i];
-7            for (int j = 0; j < dictionary.size(); j++) {
-8                String root = dictionary.get(j);
-9                if (words[i].startsWith(root)) {
-10                    if (root.length() < replacement.length()) {
-11                        replacement = root;
-12                    }
-13                }
-14            }
-15            s += replacement;
-16            if (i != words.length - 1) {
-17                s += " ";
-18            }
-19        }
-20        return s;
-21    }
-22}
-23
+5
+6        for (int i = 0; i < words.length; i++) {
+7            String replacement = words[i];
+8            for (int j = 0; j < dictionary.size(); j++) {
+9                String root = dictionary.get(j);
+10                if (words[i].startsWith(root)) {
+11                    if (root.length() < replacement.length()) {
+12                        replacement = root;
+13                    }
+14                }
+15            }
+16            s += replacement;
+17            if (i != words.length - 1) {
+18                s += " ";
+19            }
+20        }
+21        return s;
+22    }
+23}
