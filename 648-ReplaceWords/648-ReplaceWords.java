ac78a599308b5afd0a433739b53a8e5c3a63c2a4
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-02-05 12:08:44 +0530

    File updated at 2/5/2026, 12:08:07 PM

--- 648-ReplaceWords/648-ReplaceWords.java
+++ 648-ReplaceWords/648-ReplaceWords.java
@@ -1,23 +1,24 @@
-// Last updated: 8/4/2025, 6:54:15 PM
-class Solution {
-    public String replaceWords(List<String> dictionary, String sentence) {
-        String s = "";
-        String[] words = sentence.split(" ");
-        for (int i = 0; i < words.length; i++) {
-            String replacement = words[i];
-            for (int j = 0; j < dictionary.size(); j++) {
-                String root = dictionary.get(j);
-                if (words[i].startsWith(root)) {
-                    if (root.length() < replacement.length()) {
-                        replacement = root;
-                    }
-                }
-            }
-            s += replacement;
-            if (i != words.length - 1) {
-                s += " ";
-            }
-        }
-        return s;
-    }
-}
+// Last updated: 2/5/2026, 12:08:07 PM
+1class Solution {
+2    public String replaceWords(List<String> dictionary, String sentence) {
+3        String s = "";
+4        String[] words = sentence.split(" ");
+5        for (int i = 0; i < words.length; i++) {
+6            String replacement = words[i];
+7            for (int j = 0; j < dictionary.size(); j++) {
+8                String root = dictionary.get(j);
+9                if (words[i].startsWith(root)) {
+10                    if (root.length() < replacement.length()) {
+11                        replacement = root;
+12                    }
+13                }
+14            }
+15            s += replacement;
+16            if (i != words.length - 1) {
+17                s += " ";
+18            }
+19        }
+20        return s;
+21    }
+22}
+23
