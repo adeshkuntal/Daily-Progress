d5fe961883cfab0bf2f6d68cbb2d696e94b7fdd5
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-02-06 21:37:16 +0530

    File updated at 2/6/2026, 9:36:39 PM

--- 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.java
+++ 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.java
@@ -1,28 +1,27 @@
-// Last updated: 2/6/2026, 9:30:13 PM
+// Last updated: 2/6/2026, 9:36:39 PM
 1class Solution {
-2    public int[] findErrorNums(int[] nums) {
+2    public int firstMissingPositive(int[] nums) {
 3        int n = nums.length;
 4        int i = 0;
-5        while (i < n) {
-6            int correct = nums[i] - 1;
-7            if (nums[i] != nums[correct]) {
-8                int temp = nums[i];
-9                nums[i] = nums[correct];
-10                nums[correct] = temp;
-11            } else {
-12                i++;
-13            }
-14        }
-15
-16        int[] ans = new int[2];
+5
+6        while (i < n) {
+7            int correct = nums[i] - 1;
+8            if (nums[i] > 0 && nums[i] <= n && nums[i] != nums[correct]) {
+9                int temp = nums[i];
+10                nums[i] = nums[correct];
+11                nums[correct] = temp;
+12            } else {
+13                i++;
+14            }
+15        }
+16
 17        for (int j = 0; j < n; j++) {
 18            if (nums[j] != j + 1) {
-19                ans[0] = nums[j];
-20                ans[1] = j + 1;
-21                break;
-22            }
-23        }
-24        return ans;
-25    }
-26}
-27
+19                return j + 1;
+20            }
+21        }
+22
+23        return n + 1;
+24    }
+25}
+26
