d81dd2b58fee6a2dbc8f005c7b7b1c449985b675
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-02-06 21:30:50 +0530

    File updated at 2/6/2026, 9:30:13 PM

--- 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.java
+++ 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.java
@@ -1,25 +1,28 @@
-// Last updated: 2/6/2026, 9:13:27 PM
+// Last updated: 2/6/2026, 9:30:13 PM
 1class Solution {
-2    public int findDuplicate(int[] nums) {
+2    public int[] findErrorNums(int[] nums) {
 3        int n = nums.length;
 4        int i = 0;
-5        while(i < n){
-6            int correct = nums[i]-1;
-7            if(nums[i] != nums[correct]){
+5        while (i < n) {
+6            int correct = nums[i] - 1;
+7            if (nums[i] != nums[correct]) {
 8                int temp = nums[i];
 9                nums[i] = nums[correct];
 10                nums[correct] = temp;
-11            }
-12            else{
-13                i++;
-14            }
-15        }
-16        int ans = 0;
-17        for(int j=0; j<n; j++){
-18            if(nums[j] != j+1){
-19                ans = nums[j];
-20            }
-21        }
-22        return ans;
-23    }
-24}
+11            } else {
+12                i++;
+13            }
+14        }
+15
+16        int[] ans = new int[2];
+17        for (int j = 0; j < n; j++) {
+18            if (nums[j] != j + 1) {
+19                ans[0] = nums[j];
+20                ans[1] = j + 1;
+21                break;
+22            }
+23        }
+24        return ans;
+25    }
+26}
+27
