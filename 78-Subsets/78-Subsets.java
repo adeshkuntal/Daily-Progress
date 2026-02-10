46d1db19db3914f4c055edfd95c3ff0b3a9d24e4
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-02-10 21:14:02 +0530

    File updated at 2/10/2026, 9:13:25 PM

--- 78-Subsets/78-Subsets.java
+++ 78-Subsets/78-Subsets.java
@@ -1,22 +1,36 @@
-// Last updated: 10/5/2025, 7:14:17 PM
-import java.util.*;
-
-class Solution {
-    public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        findSubset(nums, res, new ArrayList<>(), 0);
-        return res;
-    }
-
-    public static void findSubset(int[] nums, List<List<Integer>> res, List<Integer> l, int idx) {
-        if (idx == nums.length) {
-            res.add(new ArrayList<>(l));
-            return;
-        }
-
-        l.add(nums[idx]);
-        findSubset(nums, res, l, idx + 1);
-        l.remove(l.size() - 1); 
-        findSubset(nums, res, l, idx + 1);
-    }
-}
+// Last updated: 2/10/2026, 9:13:25 PM
+1class Solution {
+2    public List<List<Integer>> subsets(int[] nums) {
+3        List<List<Integer>> res = new ArrayList<>();
+4        findSubset(nums, res, new ArrayList<>(), 0);
+5        return res;
+6    }
+7    public static void findSubset(int[] nums, List<List<Integer>> res, List<Integer> l, int idx) {
+8        if(idx == nums.length){
+9            res.add(new ArrayList<>(l));
+10            return;
+11        }
+12        l.add(nums[idx]);
+13        findSubset(nums,res,l,idx+1);
+14        l.remove(l.size()-1);
+15        findSubset(nums,res,l,idx+1);
+16    }
+17}
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
