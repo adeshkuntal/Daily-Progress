976798d6477c015304f3ab2aa37986885fffd402
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-02-11 19:03:39 +0530

    File updated at 2/11/2026, 7:03:03 PM

--- 70-ClimbingStairs/70-ClimbingStairs.java
+++ 70-ClimbingStairs/70-ClimbingStairs.java
@@ -1,22 +1,28 @@
-// Last updated: 1/3/2026, 12:48:41 PM
+// Last updated: 2/11/2026, 7:03:03 PM
 1class Solution {
-2    public int rob(int[] nums) {
-3        int[] dp = new int[nums.length];
-4        Arrays.fill(dp, -1);
-5        return Robber(nums, 0, dp);
+2    public int climbStairs(int n) {
+3        int[] dp = new int[n+1];
+4        Arrays.fill(dp,-1);
+5        return fn(n,dp);
 6    }
-7    public int Robber(int[] nums, int i, int[] dp) {
-8        if(i >= nums.length) {
-9            return 0;
+7    public int fn(int n,int[] dp) {
+8        if (n == 0) {
+9            return 1;
 10        }
-11        if(dp[i] != -1) {
-12            return dp[i];
+11        if(dp[n] != -1){
+12            return dp[n];
 13        }
-14        int rob = nums[i] + Robber(nums,i+2,dp);
-15        int donotRob = Robber(nums,i+1,dp);
+14        int oneStep = 0;
+15        int twoStep = 0;
 16
-17        dp[i] = Math.max(rob,donotRob);
-18
-19        return dp[i];
-20    }
-21}
+17        if (n >= 1) {
+18            oneStep = fn(n - 1,dp);
+19        }
+20        if (n >= 2) {
+21            twoStep = fn(n - 2,dp);
+22        }
+23
+24        return dp[n] = oneStep + twoStep;
+25    }
+26}
+27
