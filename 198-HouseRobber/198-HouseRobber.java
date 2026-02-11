525bf13a732af0f266e7a01a4dbcd33444e875e5
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-02-11 19:13:40 +0530

    File updated at 2/11/2026, 7:13:04 PM

--- 198-HouseRobber/198-HouseRobber.java
+++ 198-HouseRobber/198-HouseRobber.java
@@ -1,20 +1,27 @@
-// Last updated: 9/3/2025, 11:33:47 AM
-class Solution {
-    public int rob(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, -1);
-        return Robber(nums, 0, dp);
-    }
-    public int Robber(int[] nums, int i, int[] dp) {
-        if(i >= nums.length) {
-            return 0;
-        }
-        if(dp[i] != -1) {
-            return dp[i];
-        }
-        int rob = nums[i] + Robber(nums, i + 2, dp);
-        int dont_rob = Robber(nums, i + 1, dp);
-
-        return dp[i] = Math.max(rob, dont_rob);
-    }
-}
+// Last updated: 2/11/2026, 7:13:04 PM
+1class Solution {
+2    public int rob(int[] nums){
+3        int[] dp = new int[nums.length];
+4        Arrays.fill(dp,-1);
+5        return money(nums,0,dp);
+6        
+7    }
+8    public int money(int[] nums,int idx,int[] dp){
+9        if(idx >= nums.length){
+10            return 0;
+11        }
+12        if(dp[idx] != -1){
+13            return dp[idx];
+14        }
+15        int robb = nums[idx] + money(nums,idx+2,dp);
+16        int not_robb = money(nums,idx+1,dp);
+17
+18        dp[idx] = Math.max(robb,not_robb);
+19
+20        return dp[idx];
+21    }
+22}
+23
+24
+25
+26
