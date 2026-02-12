2ea8d5c1048390d4630896908392ebeb91df4d44
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-02-12 12:49:31 +0530

    File updated at 2/12/2026, 12:48:56 PM

--- 416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.java
+++ 416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.java
@@ -1,18 +1,40 @@
-// Last updated: 1/3/2026, 4:00:09 PM
-1class Solution {
-2    public boolean canPartition(int[] nums) {
-3        int totalSum = 0;
-4        for (int num : nums) totalSum += num;
-5        if (totalSum % 2 != 0) return false;
-6        int targetSum = totalSum / 2;
-7        boolean[] dp = new boolean[targetSum + 1];
-8        dp[0] = true;
-9        for (int num : nums) {
-10            for (int currSum = targetSum; currSum >= num; currSum--) {
-11                dp[currSum] = dp[currSum] || dp[currSum - num];
-12                if (dp[targetSum]) return true;
-13            }
-14        }
-15        return dp[targetSum];
-16    }
-17}
+// Last updated: 2/12/2026, 12:48:56 PM
+1// class Solution {
+2//     public int[] dailyTemperatures(int[] t) {
+3//         int[] ans = new int[t.length];
+4//         for(int i=0; i<t.length; i++){
+5//             boolean flag = false;
+6//             int count = 1;
+7//             for(int j=i+1; j<t.length; j++){
+8//                 if(t[j] > t[i]){
+9//                     ans[i] = count;
+10//                     flag = true;
+11//                     break;
+12//                 }
+13//                 count++;
+14//             }
+15//             if(!flag){
+16//                 ans[i] = 0;
+17//             }
+18//         }
+19//         return ans;
+20//     }
+21// }
+22
+23
+24class Solution {
+25    public int[] dailyTemperatures(int[] temp) {
+26        int[] ans = new int[temp.length];
+27        Stack<Integer> stack = new Stack<>();
+28
+29        for (int i = 0; i < temp.length; i++) {
+30            while (!stack.isEmpty() && temp[i] > temp[stack.peek()]) {
+31                int prevIndex = stack.pop();
+32                ans[prevIndex] = i - prevIndex;
+33            }
+34            stack.push(i);
+35        }
+36
+37        return ans;
+38    }
+39}
