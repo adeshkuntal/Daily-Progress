b11fbb9234c58060278aab49bcb4773b083beb15
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-02-17 19:40:31 +0530

    File updated at 2/17/2026, 7:39:58 PM

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.java
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.java
@@ -1,4 +1,4 @@
-// Last updated: 2/17/2026, 7:39:26 PM
+// Last updated: 2/17/2026, 7:39:58 PM
 1class Solution {
 2    public int maxArea(int[] h) {
 3        int left = 0;
@@ -8,15 +8,14 @@
 7            int a = (right-left)*Math.min(h[left],h[right]);
 8            if(a > area){
 9                area = a;
-10              
-11            }
-12            else if(h[left] < h[right]){
-13                left++;
-14            }
-15            else{
-16                right--;
-17            }
-18        }
-19        return area;
-20    }
-21}
+10            }
+11            else if(h[left] < h[right]){
+12                left++;
+13            }
+14            else{
+15                right--;
+16            }
+17        }
+18        return area;
+19    }
+20}
