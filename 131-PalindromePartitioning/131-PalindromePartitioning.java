15af5302321027c784984e246a422230958ccd56
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-02-20 22:01:47 +0530

    File updated at 2/20/2026, 10:01:15 PM

--- 131-PalindromePartitioning/131-PalindromePartitioning.java
+++ 131-PalindromePartitioning/131-PalindromePartitioning.java
@@ -1,35 +1,33 @@
-// Last updated: 2/20/2026, 9:32:54 PM
+// Last updated: 2/20/2026, 10:01:15 PM
 1class Solution {
-2    public List<List<String>> partition(String s) {
-3        List<List<String>> res = new ArrayList<>();
-4        List<String> l = new ArrayList<>();
-5        find_partition(res,l,s);
-6        return res;
-7    }
-8    public void find_partition(List<List<String>> res,List<String> ans,String ques){
-9        if(ques.length() == 0){
-10            res.add(new ArrayList<>(ans));
-11            return;
-12        }
-13        for(int i=1; i<=ques.length(); i++){
-14            String s = ques.substring(0,i);
-15            if(isPalindrome(s)){
-16                ans.add(s);
-17                find_partition(res,ans,ques.substring(i));
-18                ans.remove(ans.size() - 1);
-19            }
-20        }
-21    }
-22    public boolean isPalindrome(String s){
-23        int i = 0;
-24        int j = s.length() - 1;
-25        while(i < j){
-26            if(s.charAt(i) != s.charAt(j)){
-27                return false;
-28            }
-29            i++;
-30            j--;
-31        }
-32        return true;
-33    }
-34}
+2    public int getMaximumGold(int[][] grid) {
+3        int res = 0;
+4        for(int i=0; i<grid.length; i++){
+5            for(int j=0; j<grid[0].length; j++){
+6                if(grid[i][j] != 0){
+7                    int ans = dfs(grid,i,j);
+8                    res = Math.max(res,ans);
+9                }
+10            }
+11        }
+12        return res;
+13    }
+14
+15    public int dfs(int[][] grid,int i,int j){
+16        if(i<0 || i>= grid.length || j<0 || j>= grid[0].length || grid[i][j]==0){
+17            return 0;
+18        }
+19
+20        int gold = grid[i][j];
+21        grid[i][j] = 0;
+22
+23        int top = dfs(grid,i - 1, j);
+24        int right = dfs(grid,i, j + 1);
+25        int bottom = dfs(grid,i + 1, j);
+26        int left = dfs(grid,i, j - 1);
+27
+28        grid[i][j] = gold;
+29
+30        return gold + Math.max(top,Math.max(right,Math.max(bottom,left)));
+31    }
+32}
