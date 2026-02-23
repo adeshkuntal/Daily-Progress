1c61e17842e3d29e4b7dad9443b37c9ce95433dc
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-02-23 21:48:49 +0530

    File updated at 2/23/2026, 9:48:19 PM

--- 46-Permutations/46-Permutations.java
+++ 46-Permutations/46-Permutations.java
@@ -1,35 +1,49 @@
-// Last updated: 2/23/2026, 9:22:56 PM
+// Last updated: 2/23/2026, 9:48:19 PM
 1class Solution {
-2    public boolean predictTheWinner(int[] nums) {
-3        int n = nums.length;
-4        int sum = 0;
-5        for(int i = 0; i < n; i++) {
-6            sum += nums[i];
-7        }
-8        int[][] dp = new int[n][n];
-9        for(int[] d : dp) {
-10            Arrays.fill(d, -1);
-11        }
-12        int player1 = Fn(0, n - 1, nums, dp);
-13        int player2 = sum - player1;
-14
-15        return player1 >= player2;
-16    }
+2    public int uniquePathsIII(int[][] grid) {
+3        int empty = 0;
+4        int startRow = 0, startCol = 0;
+5
+6        for(int i=0; i<grid.length; i++){
+7            for(int j=0; j<grid[0].length; j++){
+8                if(grid[i][j] != -1){
+9                    empty++;
+10                }
+11                if(grid[i][j] == 1){
+12                    startRow = i;
+13                    startCol = j;
+14                }
+15            }
+16        }
 17
-18    public int Fn(int i, int j, int[] nums, int[][] dp) {
-19        if(i > j) {
-20            return 0;
-21        }
-22        if(i == j) {
-23            return nums[i];
+18        return dfs(grid, startRow, startCol, empty);
+19    }
+20    
+21    public int dfs(int[][] grid,int i,int j,int empty){
+22        if(i<0 || i>= grid.length || j<0 || j>= grid[0].length || grid[i][j] == -1){
+23            return 0;
 24        }
-25        if(dp[i][j] != -1) {
-26            return dp[i][j];
-27        }
-28
-29        int a = nums[i] + Math.min(Fn(i + 2, j, nums, dp), Fn(i + 1, j - 1, nums, dp));
-30        int b = nums[j] + Math.min(Fn(i, j - 2, nums, dp), Fn(i + 1, j - 1, nums, dp));
-31
-32        return dp[i][j] = Math.max(a, b);
-33    }
-34}
+25
+26        if(grid[i][j] == 2){
+27            if(empty == 1){
+28                return 1;
+29            } else {
+30                return 0;
+31            }
+32        }
+33
+34        int temp = grid[i][j];
+35        grid[i][j] = -1;
+36
+37        int total = 0;
+38
+39        total += dfs(grid,i - 1, j, empty - 1);
+40        total += dfs(grid,i, j + 1, empty - 1);
+41        total += dfs(grid,i + 1, j, empty - 1);
+42        total += dfs(grid,i, j - 1, empty - 1);
+43
+44        grid[i][j] = temp;
+45
+46        return total;
+47    }
+48}
