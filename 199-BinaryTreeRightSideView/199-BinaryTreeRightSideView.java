da0b5fe8e62b2bbe8e92678ec9d8be75fe2a98a2
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-02-24 19:02:34 +0530

    File updated at 2/24/2026, 7:02:04 PM

--- 199-BinaryTreeRightSideView/199-BinaryTreeRightSideView.java
+++ 199-BinaryTreeRightSideView/199-BinaryTreeRightSideView.java
@@ -1,4 +1,4 @@
-// Last updated: 2/24/2026, 6:54:19 PM
+// Last updated: 2/24/2026, 7:02:04 PM
 1/**
 2 * Definition for a binary tree node.
 3 * public class TreeNode {
@@ -15,31 +15,24 @@
 14 * }
 15 */
 16class Solution {
-17    public List<Integer> rightSideView(TreeNode root) {
-18        List<Integer> l = new ArrayList<>();
-19        if(root == null){
-20            return l;
-21        }
-22
-23        Queue<TreeNode> q = new LinkedList<>();
-24        q.add(root);
-25
-26        while(!q.isEmpty()){
-27            int size = q.size();
-28            for(int i = 0; i < size; i++){
-29                TreeNode temp = q.poll();
-30                if(i == size - 1){
-31                    l.add(temp.val);
-32                }
-33                if(temp.left != null){
-34                    q.add(temp.left);
-35                }
-36                if(temp.right != null){
-37                    q.add(temp.right);
-38                }
-39            }
-40        }
-41
-42        return l;
-43    }
-44}
+17    public boolean isSymmetric(TreeNode root) {
+18        if(root == null){
+19            return true;
+20        }
+21        return check(root.left, root.right);
+22    }
+23
+24    public boolean check(TreeNode a, TreeNode b){
+25        if(a == null && b == null){
+26            return true;
+27        }
+28        if(a == null || b == null){
+29            return false;
+30        }
+31        if(a.val != b.val){
+32            return false;
+33        }
+34
+35        return check(a.left, b.right) && check(a.right, b.left);
+36    }
+37}
