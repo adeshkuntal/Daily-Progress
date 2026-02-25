7a501cb6b34fad201080471bc4b21be7178d115c
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-02-25 19:40:55 +0530

    File updated at 2/25/2026, 7:40:26 PM

--- 49-GroupAnagrams/49-GroupAnagrams.java
+++ 49-GroupAnagrams/49-GroupAnagrams.java
@@ -1,27 +1,20 @@
-// Last updated: 2/25/2026, 7:30:55 PM
-1import java.util.*;
-2
-3class Solution {
-4    public List<List<String>> groupAnagrams(String[] s) {
-5        HashMap<String, List<String>> map = new HashMap<>();
-6        for (int i = 0; i < s.length; i++) {
-7            char[] arr = s[i].toCharArray();
-8            Arrays.sort(arr);
-9            String x = new String(arr);
-10            
-11            if (!map.containsKey(x)) {
-12                map.put(x, new ArrayList<>());
-13            }
-14            
-15            map.get(x).add(s[i]);
+// Last updated: 2/25/2026, 7:40:26 PM
+1class Solution {
+2    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+3        int len = flowerbed.length;
+4        int i = 0;
+5        while(i < len && n > 0) {
+6            if(flowerbed[i] == 1){
+7                i += 2;
+8            }
+9            else if(i == len - 1 || flowerbed[i + 1] == 0) {
+10                n--;
+11                i += 2;
+12            }
+13            else{
+14                i += 3;
+15            }
 16        }
-17        
-18        List<List<String>> res = new ArrayList<>();
-19        
-20        for (List<String> list : map.values()) {
-21            res.add(list);
-22        }
-23        
-24        return res;
-25    }
-26}
+17        return n <= 0;
+18    }
+19}
