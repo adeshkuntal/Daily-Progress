a0ff6796d8a1e43ea58d88f7b75b027cb8c3a2c1
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-02-25 20:01:31 +0530

    File updated at 2/25/2026, 8:01:01 PM

--- 49-GroupAnagrams/49-GroupAnagrams.java
+++ 49-GroupAnagrams/49-GroupAnagrams.java
@@ -1,20 +1,24 @@
-// Last updated: 2/25/2026, 7:45:57 PM
+// Last updated: 2/25/2026, 8:01:01 PM
 1class Solution {
-2    public boolean canPlaceFlowers(int[] flowerbed, int n) {
-3        int len = flowerbed.length;
-4        int i = 0;
-5        while(i < len && n > 0) {
-6            if(flowerbed[i] == 1){
-7                i += 2;
-8            }
-9            else if(i == len - 1 || flowerbed[i + 1] == 0) {
-10                n--;
-11                i += 2;
+2    public long minimumTime(int[] time, int totalTrips) {
+3        Arrays.sort(time);
+4
+5        long lo = 1;
+6        long hi = (long)time[0] * totalTrips;
+7        while(lo < hi){
+8            long mid = lo + (hi - lo) / 2;
+9            long count = 0;
+10            for (int t : time) {
+11                count += mid / t;
 12            }
-13            else{
-14                i += 3;
+13            if(count >= totalTrips){
+14                hi = mid;
 15            }
-16        }
-17        return n <= 0;
-18    }
-19}
+16            else{
+17                lo = mid + 1;
+18            }
+19        }
+20
+21        return lo;
+22    }
+23}
