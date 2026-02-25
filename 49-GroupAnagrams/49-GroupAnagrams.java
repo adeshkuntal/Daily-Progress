b0989cbee3e9829c2c9a9eae4f15c0ff99df96e2
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-02-25 20:02:31 +0530

    File updated at 2/25/2026, 8:02:02 PM

--- 49-GroupAnagrams/49-GroupAnagrams.java
+++ 49-GroupAnagrams/49-GroupAnagrams.java
@@ -1,24 +1,24 @@
-// Last updated: 2/25/2026, 8:01:01 PM
+// Last updated: 2/25/2026, 8:02:02 PM
 1class Solution {
-2    public long minimumTime(int[] time, int totalTrips) {
+2    public long minimumTime(int[] time, int t) {
 3        Arrays.sort(time);
-4
-5        long lo = 1;
-6        long hi = (long)time[0] * totalTrips;
-7        while(lo < hi){
-8            long mid = lo + (hi - lo) / 2;
-9            long count = 0;
-10            for (int t : time) {
-11                count += mid / t;
-12            }
-13            if(count >= totalTrips){
-14                hi = mid;
-15            }
-16            else{
-17                lo = mid + 1;
-18            }
+4        long l = 1;
+5        long r = (long)t*time[0];
+6        while(l < r){
+7            long mid = l+(r-l)/2;
+8            long count = 0;
+9            for(int i=0; i<time.length; i++){
+10                count += mid/time[i];
+11            }
+12            if(count >= t){
+13                r = mid;
+14            }
+15            else{
+16                l = mid+1;
+17            }
+18
 19        }
 20
-21        return lo;
+21        return l;
 22    }
 23}
