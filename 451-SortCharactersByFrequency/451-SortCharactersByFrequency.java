9bdc3cbf03df6e9427a57f83d67c75cb74f2e094
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-02-26 16:21:08 +0530

    File updated at 2/26/2026, 4:20:39 PM

--- 451-SortCharactersByFrequency/451-SortCharactersByFrequency.java
+++ 451-SortCharactersByFrequency/451-SortCharactersByFrequency.java
@@ -1,40 +1,38 @@
-// Last updated: 10/6/2025, 10:16:42 PM
-
-class Solution {
-    public String frequencySort(String s) {
-        int count = 0;
-        int max = 0;
-        String ans = "";
-
-        // Count frequency of each character
-        Map<Character, Integer> freq = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
-            char a = s.charAt(i);
-            freq.put(a, freq.getOrDefault(a, 0) + 1);
-            if (freq.get(a) > max){
-                max = freq.get(a);
-            }
-        }
-
-        // Prepare buckets where index = frequency
-        List<List<Character>> buckets = new ArrayList<>();
-        for (int i = 0; i <= max; i++){
-            buckets.add(new ArrayList<>());
-        }
-
-        // Fill characters into frequency buckets
-        for (char ch : freq.keySet()) {
-            int f = freq.get(ch);
-            buckets.get(f).add(ch);
-        }
-
-        // Build answer from highest freq to lowest
-        for (int i = max; i >= 1; i--) {
-            for (char ch : buckets.get(i)) {
-                ans += String.valueOf(ch).repeat(i);
-            }
-        }
-
-        return ans;
-    }
-}
+// Last updated: 2/26/2026, 4:20:39 PM
+1import java.util.*;
+2
+3class Solution {
+4    public String frequencySort(String s) {
+5        int max = 0;
+6        HashMap<Character, Integer> map = new HashMap<>();
+7        for(int i=0; i<s.length(); i++){
+8            char ch = s.charAt(i);
+9            map.put(ch,map.getOrDefault(ch,0)+1);
+10            if(map.get(ch) > max){
+11                max = map.get(ch);
+12            }
+13        }
+14
+15        List<List<Character>> l = new ArrayList<>();
+16        for (int i = 0; i <= max; i++){
+17            l.add(new ArrayList<>());
+18        }
+19
+20        for (char ch : map.keySet()){
+21            int f = map.get(ch);
+22            l.get(f).add(ch);
+23        }
+24
+25        StringBuilder ans = new StringBuilder();
+26
+27        for(int i=max; i>=1; i--){
+28            for(char ch : l.get(i)){
+29                for(int j=0; j<i; j++){
+30                    ans.append(ch);
+31                }
+32            }
+33        }
+34
+35        return ans.toString();
+36    }
+37}
