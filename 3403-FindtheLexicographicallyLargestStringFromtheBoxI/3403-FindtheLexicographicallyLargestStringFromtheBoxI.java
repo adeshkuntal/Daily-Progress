eafc9c9d6da05f19521ea4b4f053bb782650770f
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-02-26 14:33:15 +0530

    File updated at 2/26/2026, 2:32:46 PM

--- 3403-FindtheLexicographicallyLargestStringFromtheBoxI/3403-FindtheLexicographicallyLargestStringFromtheBoxI.java
+++ 3403-FindtheLexicographicallyLargestStringFromtheBoxI/3403-FindtheLexicographicallyLargestStringFromtheBoxI.java
@@ -1,23 +1,23 @@
-// Last updated: 10/6/2025, 10:06:57 PM
-class Solution {
-    public String answerString(String word, int numFriends) {
-        if (numFriends == 1) return word;
-
-        String maxP1 = "";
-        int maxLength = word.length() - numFriends + 1;
-
-        for (int i = 0; i < word.length(); i++) {
-            String temp = "";
-            if (i + maxLength <= word.length()) {
-                temp = word.substring(i, i + maxLength);
-            } else {
-                temp = word.substring(i);
-            }
-
-            if (temp.compareTo(maxP1) > 0) {
-                maxP1 = temp;
-            }
-        }
-        return maxP1;
-    }
-}
+// Last updated: 2/26/2026, 2:32:46 PM
+1class Solution {
+2    public String answerString(String word, int numFriends) {
+3        if (numFriends == 1) return word;
+4
+5        String maxP1 = "";
+6        int maxLength = word.length() - numFriends + 1;
+7
+8        for (int i = 0; i < word.length(); i++) {
+9            String temp = "";
+10            if (i + maxLength <= word.length()) {
+11                temp = word.substring(i, i + maxLength);
+12            } else {
+13                temp = word.substring(i);
+14            }
+15
+16            if (temp.compareTo(maxP1) > 0) {
+17                maxP1 = temp;
+18            }
+19        }
+20        return maxP1;
+21    }
+22}
