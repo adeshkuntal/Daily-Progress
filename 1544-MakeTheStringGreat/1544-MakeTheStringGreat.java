39da5d23246f12cd2c10663464ba66d20fa3a4cc
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-02-26 22:15:26 +0530

    File updated at 2/26/2026, 10:14:57 PM

--- 1544-MakeTheStringGreat/1544-MakeTheStringGreat.java
+++ 1544-MakeTheStringGreat/1544-MakeTheStringGreat.java
@@ -1,22 +1,32 @@
-// Last updated: 2/26/2026, 9:52:19 PM
-1class Solution {
-2    public int maximum69Number (int num) {
-3        String s = String.valueOf(num);
-4        int[] arr = new int[s.length()];
-5        for(int i = 0; i < s.length(); i++){
-6            arr[i] = s.charAt(i) - '0';
-7        }
-8        for(int i=0; i<arr.length; i++){
-9            if(arr[i] == 6){
-10                arr[i] = 9;
-11                break;
-12            }
-13        }
-14
-15        int n = 0;
-16        for(int j = 0; j < arr.length; j++){
-17            n = n * 10 + arr[j];
-18        }
-19        return n;
-20    }
-21}
+// Last updated: 2/26/2026, 10:14:57 PM
+1import java.util.*;
+2
+3class Solution {
+4    public int maximumBags(int[] cap, int[] rock, int addRocks) {
+5        int n = cap.length;
+6        int[] remain = new int[n];
+7        
+8        for(int i = 0; i < n; i++){
+9            remain[i] = cap[i] - rock[i];
+10        }
+11        
+12        Arrays.sort(remain);
+13        
+14        int count = 0;
+15        
+16        for(int i = 0; i < n; i++){
+17            if(remain[i] == 0){
+18                count++;
+19            }
+20            else if(addRocks >= remain[i]){
+21                addRocks -= remain[i];
+22                count++;
+23            }
+24            else{
+25                break;
+26            }
+27        }
+28        
+29        return count;
+30    }
+31}
