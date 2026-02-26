c1bbf2953cb5a8144627ef02f62f0660024c5a2d
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-02-26 21:26:26 +0530

    File updated at 2/26/2026, 9:25:57 PM

--- 1544-MakeTheStringGreat/1544-MakeTheStringGreat.java
+++ 1544-MakeTheStringGreat/1544-MakeTheStringGreat.java
@@ -1,23 +1,14 @@
-// Last updated: 1/19/2026, 10:27:55 AM
+// Last updated: 2/26/2026, 9:25:57 PM
 1class Solution {
 2    public String makeGood(String s) {
-3        StringBuilder sb = new StringBuilder(s);
-4        int i = 0;
-5        while (i<sb.length() - 1) {
-6            char c1 = sb.charAt(i);
-7            char c2 = sb.charAt(i + 1);
-8            if (Character.toLowerCase(c1) == Character.toLowerCase(c2) && c1 != c2) {
-9                sb.delete(i,i+2);
-10                if(i>0){
-11                    i--;
-12                }
-13            } 
-14            else {
-15                i++;
-16            }
-17        }
-18        return sb.toString();
-19    }
-20}
-21
-22
+3        StringBuilder str = new StringBuilder(s);
+4        for(int i = 0; i < str.length() - 1; i++){
+5            if(Math.abs(str.charAt(i) - str.charAt(i+1)) == 32){
+6                str.delete(i, i+2);
+7                i = -1;
+8            }
+9        }
+10        
+11        return str.toString();
+12    }
+13}
