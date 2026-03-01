45e2e4d61071669b0873b32e2fae7ee0466eded8
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-03-01 22:19:50 +0530

    Create

---
+++ problem-1772383761368/problem-1772383761368.java
@@ -0,0 +1,31 @@
+// Last updated: 3/1/2026, 10:19:21 PM
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    TreeNode nextNode = null;
+18    public void flatten(TreeNode root) {
+19        if(root == null){
+20            return;
+21        }
+22        flatten(root.right);
+23        flatten(root.left);
+24
+25        root.left = null;
+26        root.right = nextNode;
+27        nextNode = root;
+28
+29    }
+30}
