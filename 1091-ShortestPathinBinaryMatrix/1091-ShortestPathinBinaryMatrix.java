1d84434a122c04a5e68e502cf3552f65347aac72
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-03-07 21:52:00 +0530

    File updated at 3/7/2026, 9:51:33 PM

--- 1091-ShortestPathinBinaryMatrix/1091-ShortestPathinBinaryMatrix.java
+++ 1091-ShortestPathinBinaryMatrix/1091-ShortestPathinBinaryMatrix.java
@@ -1,41 +1,49 @@
-// Last updated: 12/30/2025, 1:22:40 PM
+// Last updated: 3/7/2026, 9:51:33 PM
 1class Solution {
 2    public int shortestPathBinaryMatrix(int[][] grid) {
 3        int n = grid.length;
-4        if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1) {
-5            return -1;
-6        }
-7
-8        int[][] directions = {
-9            {1, 0}, {-1, 0}, {0, 1}, {0, -1},
-10            {1, 1}, {1, -1}, {-1, 1}, {-1, -1}
-11        };
-12
-13        boolean[][] visited = new boolean[n][n];
-14        Queue<int[]> queue = new LinkedList<>();
-15        queue.add(new int[] {0, 0, 1}); // row, col, distance
-16        visited[0][0] = true;
-17
-18        while (!queue.isEmpty()) {
-19            int[] cur = queue.poll();
-20            int r = cur[0], c = cur[1], dist = cur[2];
+4
+5        if(grid[0][0] == 1 || grid[n-1][n-1] == 1){
+6            return -1;
+7        }
+8
+9        int[][] dir = {
+10            {1,0},{-1,0},{0,1},{0,-1},
+11            {1,1},{1,-1},{-1,1},{-1,-1}
+12        };
+13
+14        Queue<int[]> q = new LinkedList<>();
+15        q.add(new int[]{0,0});
+16
+17        boolean[][] vis = new boolean[n][n];
+18        vis[0][0] = true;
+19
+20        int count = 1;
 21
-22            if (r == n - 1 && c == n - 1) {
-23                return dist;
-24            }
-25
-26            for (int[] d : directions) {
-27                int nr = r + d[0];
-28                int nc = c + d[1];
-29
-30                if (nr >= 0 && nr < n && nc >= 0 && nc < n &&
-31                    !visited[nr][nc] && grid[nr][nc] == 0) {
-32                    visited[nr][nc] = true;
-33                    queue.offer(new int[] {nr, nc, dist + 1});
-34                }
-35            }
-36        }
-37
-38        return -1;
-39    }
-40}
+22        while(!q.isEmpty()){
+23            int size = q.size();
+24
+25            for(int i=0;i<size;i++){
+26                int[] curr = q.poll();
+27
+28                if(curr[0] == n-1 && curr[1] == n-1){
+29                    return count;
+30                }
+31
+32                for(int[] d: dir){
+33                    int x = curr[0] + d[0];
+34                    int y = curr[1] + d[1];
+35
+36                    if(x>=0 && x<n && y>=0 && y<n && grid[x][y]==0 && !vis[x][y]){
+37                        vis[x][y] = true;
+38                        q.add(new int[]{x,y});
+39                    }
+40                }
+41            }
+42
+43            count++;
+44        }
+45
+46        return -1;
+47    }
+48}
