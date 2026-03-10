b2830e60272a301f04fce7554925acd6c40427f5
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-03-10 22:03:49 +0530

    File updated at 3/10/2026, 10:03:23 PM

--- 99-RecoverBinarySearchTree/99-RecoverBinarySearchTree.java
+++ 99-RecoverBinarySearchTree/99-RecoverBinarySearchTree.java
@@ -1,4 +1,4 @@
-// Last updated: 3/4/2026, 3:36:55 PM
+// Last updated: 3/10/2026, 10:03:23 PM
 1/**
 2 * Definition for a binary tree node.
 3 * public class TreeNode {
@@ -14,49 +14,31 @@
 13 *     }
 14 * }
 15 */
-16/**
-17 * Definition for a binary tree node.
-18 * public class TreeNode {
-19 *     int val;
-20 *     TreeNode left;
-21 *     TreeNode right;
-22 *     TreeNode() {}
-23 *     TreeNode(int val) { this.val = val; }
-24 *     TreeNode(int val, TreeNode left, TreeNode right) {
-25 *         this.val = val;
-26 *         this.left = left;
-27 *         this.right = right;
-28 *     }
-29 * }
-30 */
-31class Solution {
-32    TreeNode prev = null;
-33    TreeNode first = null;
-34    TreeNode sec = null;
-35
-36    public void recoverTree(TreeNode root) {
-37        inorder(root);
-38        int temp = first.val;
-39        first.val = sec.val;
-40        sec.val = temp;
-41    }
-42
-43    public void inorder(TreeNode root){
-44        if(root == null){
-45            return;
-46        }
-47
-48        inorder(root.left);
-49
-50        if(prev != null && root.val < prev.val){
-51            if(first == null){
-52                first = prev;
-53            }
-54            sec = root;
-55        }
-56
-57        prev = root;
-58
-59        inorder(root.right);
-60    }
-61}
+16class Solution {
+17    TreeNode prev = null;
+18    TreeNode first = null;
+19    TreeNode second = null;
+20    public void recoverTree(TreeNode root) {
+21        inorder(root);
+22        int temp = first.val;
+23        first.val = second.val;
+24        second.val = temp;
+25
+26    }
+27    public void inorder(TreeNode root){
+28        if(root == null){
+29            return;
+30        }
+31        inorder(root.left);
+32        if(prev != null && root.val < prev.val){
+33            if(first == null){
+34                first = prev;
+35            }
+36            second = root;
+37        }
+38
+39        prev = root;
+40
+41        inorder(root.right);
+42    }
+43}
