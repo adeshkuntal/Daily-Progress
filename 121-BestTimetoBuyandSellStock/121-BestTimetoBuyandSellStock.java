8747a8e77b76acbbc343b3370da1b04a261e20a2
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-03-10 12:42:13 +0530

    File updated at 3/10/2026, 12:41:46 PM

--- 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.java
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.java
@@ -1,17 +1,17 @@
-// Last updated: 9/26/2025, 1:51:24 PM
-class Solution {
-    public int maxProfit(int[] prices) {
-        int min_Price = Integer.MAX_VALUE;
-        int maxProfit = 0;
-        
-        for (int price : prices) {
-            if (price < min_Price) {
-                min_Price = price;
-            } else if (price - min_Price > maxProfit) {
-                maxProfit = price - min_Price;
-            }
-        }
-        
-        return maxProfit;
-    }
-}
+// Last updated: 3/10/2026, 12:41:46 PM
+1class Solution {
+2    public int maxProfit(int[] prices) {
+3        int min_Price = Integer.MAX_VALUE;
+4        int maxProfit = 0;
+5        
+6        for (int price : prices) {
+7            if (price < min_Price) {
+8                min_Price = price;
+9            } else if (price - min_Price > maxProfit) {
+10                maxProfit = price - min_Price;
+11            }
+12        }
+13        
+14        return maxProfit;
+15    }
+16}
