85352ab61110bd32df26ed9a61913b90c591e2fb
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-03-12 22:12:29 +0530

    File updated at 3/12/2026, 10:12:02 PM

--- 1192-CriticalConnectionsinaNetwork/1192-CriticalConnectionsinaNetwork.java
+++ 1192-CriticalConnectionsinaNetwork/1192-CriticalConnectionsinaNetwork.java
@@ -1,8 +1,8 @@
-// Last updated: 3/12/2026, 10:11:22 PM
+// Last updated: 3/12/2026, 10:12:02 PM
 1class Solution {
 2    public void sortColors(int[] nums) {
 3        for(int j=0; j<nums.length; j++){
-4            for(int i=1; i<nums.length; i++){
+4            for(int i=1; i<nums.length-j; i++){
 5                if(nums[i] < nums[i-1]){
 6                    int temp = nums[i];
 7                    nums[i] = nums[i-1];
