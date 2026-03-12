2c8d33f0779dec62c134b4bae6cd0fe1b8ac50bc
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-03-12 20:34:08 +0530

    File updated at 3/12/2026, 8:33:41 PM

--- 1192-CriticalConnectionsinaNetwork/1192-CriticalConnectionsinaNetwork.java
+++ 1192-CriticalConnectionsinaNetwork/1192-CriticalConnectionsinaNetwork.java
@@ -1,14 +1,21 @@
-// Last updated: 3/12/2026, 8:06:50 PM
+// Last updated: 3/12/2026, 8:33:41 PM
 1class Solution {
-2    public int maxProfit(int[] price) {
-3        int profit = 0;
-4
-5        for(int i=1;i<price.length;i++){
-6            if(price[i] > price[i-1]){
-7                profit += price[i] - price[i-1];
-8            }
-9        }
-10
-11        return profit;
-12    }
-13}
+2    public int maxArea(int[] h) {
+3        int left = 0;
+4        int right = h.length-1;
+5        int area = 0;
+6        while(left < right){
+7            int a = (right-left)*Math.min(h[left],h[right]);
+8            if(a > area){
+9                area = a;
+10            }
+11            else if(h[left] < h[right]){
+12                left++;
+13            }
+14            else{
+15                right--;
+16            }
+17        }
+18        return area;
+19    }
+20}
