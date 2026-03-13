900e42900f204f9c89f9df477f644a9fc9c9f6ad
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-03-13 21:12:50 +0530

    File updated at 3/13/2026, 9:12:25 PM

--- 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.java
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.java
@@ -1,34 +1,17 @@
-// Last updated: 3/13/2026, 9:07:35 PM
+// Last updated: 3/13/2026, 9:12:25 PM
 1class Solution {
 2    public int[] productExceptSelf(int[] nums) {
 3        int[] res = new int[nums.length];
-4        int p = 1;
-5        int zero = 0;
-6
-7        for(int i=0; i<nums.length; i++){
-8            if(nums[i] == 0){
-9                zero++;
-10            }else{
-11                p *= nums[i];
-12            }
-13        }
-14
-15        for(int i=0; i<nums.length; i++){
-16            if(zero > 1){
-17                res[i] = 0;
-18            }
-19            else if(zero == 1){
-20                if(nums[i] == 0){
-21                    res[i] = p;
-22                }else{
-23                    res[i] = 0;
-24                }
-25            }
-26            else{
-27                res[i] = p/nums[i];
-28            }
-29        }
-30
-31        return res;
-32    }
-33}
+4        res[0] = 1;
+5        for(int i=1; i<nums.length; i++){
+6            res[i] = res[i-1]*nums[i-1];
+7        }
+8        int right = 1;
+9        for(int i=nums.length-1; i>=0; i--){
+10            res[i] = res[i]*right;
+11            right = right*nums[i];
+12        }
+13        return res;
+14        
+15    }
+16}
