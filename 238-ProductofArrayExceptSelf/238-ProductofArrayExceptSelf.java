5d8c21ef87fb11b6be57afd15d975e1527e86a6a
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-03-13 21:08:01 +0530

    File updated at 3/13/2026, 9:07:35 PM

--- 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.java
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.java
@@ -1,20 +1,34 @@
-// Last updated: 9/26/2025, 11:29:39 PM
-class Solution {
-    public int[] productExceptSelf(int[] arr) {
-        int n = arr.length;
-        int[] left = new int[n];
-        int[] right = new int[n];
-        left[0] = 1;
-        for(int i=1;i<n;i++){
-            left[i] = left[i-1]*arr[i-1];
-        }
-        right[n-1] = 1;
-        for(int i=n-2;i>=0;i--){
-            right[i] = right[i+1]*arr[i+1];
-        }
-        for(int i=0;i<n;i++){
-            left[i] = left[i]*right[i];
-        }
-        return left;
-    }
-}
+// Last updated: 3/13/2026, 9:07:35 PM
+1class Solution {
+2    public int[] productExceptSelf(int[] nums) {
+3        int[] res = new int[nums.length];
+4        int p = 1;
+5        int zero = 0;
+6
+7        for(int i=0; i<nums.length; i++){
+8            if(nums[i] == 0){
+9                zero++;
+10            }else{
+11                p *= nums[i];
+12            }
+13        }
+14
+15        for(int i=0; i<nums.length; i++){
+16            if(zero > 1){
+17                res[i] = 0;
+18            }
+19            else if(zero == 1){
+20                if(nums[i] == 0){
+21                    res[i] = p;
+22                }else{
+23                    res[i] = 0;
+24                }
+25            }
+26            else{
+27                res[i] = p/nums[i];
+28            }
+29        }
+30
+31        return res;
+32    }
+33}
