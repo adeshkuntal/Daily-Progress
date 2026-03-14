76cb1d0ac5cc6251491ee0c8edca4634d631e005
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-03-14 20:13:31 +0530

    File updated at 3/14/2026, 8:13:06 PM

--- 84-LargestRectangleinHistogram/84-LargestRectangleinHistogram.java
+++ 84-LargestRectangleinHistogram/84-LargestRectangleinHistogram.java
@@ -1,32 +1,42 @@
-// Last updated: 3/14/2026, 8:12:50 PM
-1class Solution {
-2    public int largestRectangleArea(int[] height) {
-3        int n = height.length;
-4        int[] rsn = new int[n];
-5        int[] lsn = new int[n];
-6        Stack<Integer> s = new Stack<>();
-7        for(int i=0; i<n; i++){
-8            while(!s.isEmpty() && height[s.peek()] >= height[i]){
-9                s.pop();
-10            }
-11            lsn[i] = s.isEmpty() ? -1 : s.peek();
-12            s.push(i);
-13        }
-14        s.clear();
-15        for(int i=n-1; i>=0; i--){
-16            while(!s.isEmpty() && height[s.peek()] >= height[i]){
-17                s.pop();
-18            }
-19            rsn[i] = s.isEmpty() ? n : s.peek();
-20            s.push(i);
-21        }
+// Last updated: 3/14/2026, 8:13:06 PM
+1import java.util.*;
+2
+3class Solution {
+4    public int largestRectangleArea(int[] height) {
+5
+6        int n = height.length;
+7        int[] rsn = new int[n];
+8        int[] lsn = new int[n];
+9
+10        Stack<Integer> s = new Stack<>();
+11
+12        for(int i = 0; i < n; i++){
+13            while(!s.isEmpty() && height[s.peek()] >= height[i]){
+14                s.pop();
+15            }
+16
+17            lsn[i] = s.isEmpty() ? -1 : s.peek();
+18            s.push(i);
+19        }
+20
+21        s.clear();
 22
-23        int ans = 0;
-24        for(int i = 0; i<n; i++){
-25            int curr = height[i]*(rsn[i]-lsn[i]-1);
-26            ans = Math.max(ans,curr);
-27        }
-28        return ans;
-29
-30    }
-31}
+23        for(int i = n-1; i >= 0; i--){
+24            while(!s.isEmpty() && height[s.peek()] >= height[i]){
+25                s.pop();
+26            }
+27
+28            rsn[i] = s.isEmpty() ? n : s.peek();
+29            s.push(i);
+30        }
+31
+32        int ans = 0;
+33
+34        for(int i = 0; i < n; i++){
+35            int curr = height[i] * (rsn[i] - lsn[i] - 1);
+36            ans = Math.max(ans, curr);
+37        }
+38
+39        return ans;
+40    }
+41}
