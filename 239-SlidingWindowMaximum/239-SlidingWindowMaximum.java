3d1283d85b1524692fa32569ddcce5518b1750be
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-03-14 16:27:34 +0530

    File updated at 3/14/2026, 4:27:08 PM

--- 239-SlidingWindowMaximum/239-SlidingWindowMaximum.java
+++ 239-SlidingWindowMaximum/239-SlidingWindowMaximum.java
@@ -1,39 +1,40 @@
-// Last updated: 9/28/2025, 1:33:26 PM
-import java.util.*;
-
-class Solution {
-    public int[] maxSlidingWindow(int[] nums, int k) {
-        int n = nums.length;
-        Deque<Integer> dq = new ArrayDeque<>();
-        List<Integer> res = new ArrayList<>();
-
-        for (int i = 0; i < k; i++) {
-            while (!dq.isEmpty() && nums[dq.peekLast()] <= nums[i]) {
-                dq.pollLast();
-            }
-            dq.addLast(i);
-        }
-
-        for (int i = k; i < n; i++) {
-            res.add(nums[dq.peekFirst()]);
-
-            while (!dq.isEmpty() && dq.peekFirst() <= i - k) {
-                dq.pollFirst();
-            }
-
-            while (!dq.isEmpty() && nums[dq.peekLast()] <= nums[i]) {
-                dq.pollLast();
-            }
-
-            dq.addLast(i);
-        }
-
-        res.add(nums[dq.peekFirst()]);
-
-        int[] ans = new int[res.size()];
-        for (int i = 0; i < res.size(); i++) {
-            ans[i] = res.get(i);
-        }
-        return ans;
-    }
-}
+// Last updated: 3/14/2026, 4:27:08 PM
+1import java.util.*;
+2
+3class Solution {
+4    public int[] maxSlidingWindow(int[] nums, int k) {
+5        int n = nums.length;
+6        Deque<Integer> dq = new ArrayDeque<>();
+7        List<Integer> res = new ArrayList<>();
+8
+9        for (int i = 0; i < k; i++) {
+10            while (!dq.isEmpty() && nums[dq.peekLast()] <= nums[i]) {
+11                dq.pollLast();
+12            }
+13            dq.addLast(i);
+14        }
+15
+16        for (int i = k; i < n; i++) {
+17            res.add(nums[dq.peekFirst()]);
+18
+19            while (!dq.isEmpty() && dq.peekFirst() <= i - k) {
+20                dq.pollFirst();
+21            }
+22
+23            while (!dq.isEmpty() && nums[dq.peekLast()] <= nums[i]) {
+24                dq.pollLast();
+25            }
+26
+27            dq.addLast(i);
+28        }
+29
+30        res.add(nums[dq.peekFirst()]);
+31
+32        int[] ans = new int[res.size()];
+33        for (int i = 0; i < res.size(); i++) {
+34            ans[i] = res.get(i);
+35        }
+36        return ans;
+37    }
+38}
+39
