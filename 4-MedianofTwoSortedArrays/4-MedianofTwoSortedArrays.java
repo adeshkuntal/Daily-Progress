f17232085dab181b5a095718a039b732ff36820d
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-03-15 16:25:58 +0530

    File updated at 3/15/2026, 4:25:32 PM

--- 4-MedianofTwoSortedArrays/4-MedianofTwoSortedArrays.java
+++ 4-MedianofTwoSortedArrays/4-MedianofTwoSortedArrays.java
@@ -1,23 +1,35 @@
-// Last updated: 9/27/2025, 3:25:22 PM
-class Solution {
-    public double findMedianSortedArrays(int[] arr1, int[] arr2) {
-        int[] mergedArray = new int[arr1.length + arr2.length];
-        int index = 0;
-
-        for (int i = 0; i < arr1.length; i++) {
-            mergedArray[index++] = arr1[i];
-        }
-        for (int i = 0; i < arr2.length; i++) {
-            mergedArray[index++] = arr2[i];
-        }
-
-        Arrays.sort(mergedArray);
-
-        int n = mergedArray.length;
-        if (n % 2 != 0) {
-            return mergedArray[n / 2];
-        } else {
-            return (mergedArray[(n / 2) - 1] + mergedArray[n / 2]) / 2.0;
-        }
-    }
-}
+// Last updated: 3/15/2026, 4:25:32 PM
+1class Solution {
+2    public double findMedianSortedArrays(int[] arr1, int[] arr2) {
+3        int[] mergedArray = new int[arr1.length + arr2.length];
+4        int idx = 0;
+5        int i = 0, j = 0;
+6
+7        while(i < arr1.length && j < arr2.length){
+8            if(arr1[i] < arr2[j]){
+9                mergedArray[idx++] = arr1[i++];
+10            }
+11            else{
+12                mergedArray[idx++] = arr2[j++];
+13            }
+14        }
+15
+16        while(i < arr1.length){
+17            mergedArray[idx++] = arr1[i++];
+18        }
+19
+20        while(j < arr2.length){
+21            mergedArray[idx++] = arr2[j++];
+22        }
+23
+24        int n = mergedArray.length;
+25        if(n%2 != 0){
+26            return mergedArray[((n+1)/2)-1];
+27        }
+28        else{
+29            return (mergedArray[(n / 2) - 1] + mergedArray[n / 2]) / 2.0;
+30        }
+31        
+32    }
+33}
+34
