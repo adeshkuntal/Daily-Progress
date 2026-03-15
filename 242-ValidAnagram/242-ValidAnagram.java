c737297f896dca7c72506e67d81d87be81b9ce44
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-03-15 18:18:41 +0530

    File updated at 3/15/2026, 6:18:16 PM

--- 242-ValidAnagram/242-ValidAnagram.java
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -1,15 +1,12 @@
-// Last updated: 8/4/2025, 6:56:25 PM
-class Solution {
-    public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
-            return false;
-        }
-
-        char[] sArray = s.toCharArray();
-        char[] tArray = t.toCharArray();
-        Arrays.sort(sArray);
-        Arrays.sort(tArray);
-
-        return Arrays.equals(sArray, tArray);
-    }
-}
+// Last updated: 3/15/2026, 6:18:16 PM
+1class Solution {
+2    public boolean isAnagram(String s, String t) {
+3        char[] arr1 = s.toCharArray();
+4        Arrays.sort(arr1);
+5        String s1 = Arrays.toString(arr1);
+6        char[] arr2 = t.toCharArray();
+7        Arrays.sort(arr2);
+8        String s2 = Arrays.toString(arr2);
+9        return s1.equals(s2);
+10    }
+11}
