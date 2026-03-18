9d7a62910189900cbf3140e7548560c84ccb1331
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-03-18 22:17:06 +0530

    File updated at 3/18/2026, 10:16:40 PM

--- 51-N-Queens/51-N-Queens.java
+++ 51-N-Queens/51-N-Queens.java
@@ -1,4 +1,4 @@
-// Last updated: 3/2/2026, 10:28:38 PM
+// Last updated: 3/18/2026, 10:16:40 PM
 1class Solution {
 2    public List<List<String>> solveNQueens(int n) {
 3        List<List<String>> allBoards = new ArrayList<>();
@@ -9,7 +9,7 @@
 8        place_queen(board, allBoards, 0);
 9        return allBoards;
 10    }
-11    public static void place_queen(char[][] board, List<List<String>> allBoards, int col){
+11    public void place_queen(char[][] board,List<List<String>> allBoards,int col){
 12        if(col >= board.length){
 13            saveBoard(board,allBoards);
 14            return;
@@ -23,33 +23,33 @@
 22        }
 23    }
 24    public static void saveBoard(char[][] board, List<List<String>> allBoards){
-25        List<String> newBoard = new ArrayList<>();
-26        for(int row=0; row<board.length; row++){
+25        List<String> curr = new ArrayList<>();
+26        for(int i=0; i<board.length; i++){
 27            String s = "";
-28            for(int col=0; col<board.length; col++){
-29                s += board[row][col];
+28            for(int j=0; j<board.length; j++){
+29                s += board[i][j];
 30            }
-31            newBoard.add(s);
+31            curr.add(s);
 32        }
-33        allBoards.add(newBoard);
+33        allBoards.add(curr);
 34    }
-35    public static boolean isSafe(int row, int col, char[][] board){
-36        for(int r=row,c = col; r>=0 && c>=0; r--,c--){
-37            if(board[r][c] == 'Q'){
-38                return false;
-39            }
-40        }
-41        for(int r=row,c = col; r<board.length && c>=0; r++,c--){
-42            if(board[r][c] == 'Q'){
-43                return false;
-44            }
-45        }
-46        for(int c = col; c>=0; c--){
-47            if(board[row][c] == 'Q'){
-48                return false;
-49            }
-50        }
-51        return true;
-52
+35    
+36    public boolean isSafe(int row,int col,char[][] board){
+37        for(int r=row,c = col; r>=0 && c>=0; r--,c--){
+38            if(board[r][c] == 'Q'){
+39                return false;
+40            }
+41        }
+42        for(int r=row,c = col; r<board.length && c>=0; r++,c--){
+43            if(board[r][c] == 'Q'){
+44                return false;
+45            }
+46        }
+47        for(int c = col; c>=0; c--){
+48            if(board[row][c] == 'Q'){
+49                return false;
+50            }
+51        }
+52        return true;
 53    }
 54}
