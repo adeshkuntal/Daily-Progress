b57516029fe1fbd485d1b4944934624ec42600d9
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-03-20 17:47:20 +0530

    File updated at 3/20/2026, 5:46:55 PM

--- 232-ImplementQueueusingStacks/232-ImplementQueueusingStacks.java
+++ 232-ImplementQueueusingStacks/232-ImplementQueueusingStacks.java
@@ -1,20 +1,20 @@
-// Last updated: 3/20/2026, 5:45:17 PM
+// Last updated: 3/20/2026, 5:46:55 PM
 1class Solution {
 2    public boolean isValid(String s) {
-3        Stack<Character> stack = new Stack<>();
-4        for (char c : s.toCharArray()) {
-5            if (c == '(' || c == '{' || c == '[') {
-6                stack.push(c);
-7            } else {
-8                if (stack.isEmpty()) return false;
-9                char top = stack.pop();
-10                if ((c == ')' && top != '(') || 
-11                    (c == '}' && top != '{') || 
-12                    (c == ']' && top != '[')) {
-13                    return false;
-14                }
-15            }
-16        }
-17        return stack.isEmpty();
+3        Stack<Character> st = new Stack<>();
+4        for(int i=0; i<s.length(); i++){
+5            if(s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '['){
+6                st.push(s.charAt(i));
+7            }
+8            else{
+9                if (st.isEmpty()) return false;
+10                char ch = st.pop();
+11                if((s.charAt(i) == ')' && ch != '(') || (s.charAt(i) == '}' && ch != '{') || (s.charAt(i) == ']' && ch != '[')){
+12                    return false;
+13                }
+14            }
+15        }
+16
+17        return st.isEmpty();
 18    }
 19}
