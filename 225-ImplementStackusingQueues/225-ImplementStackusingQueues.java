53b78567adda0299dc2be058ab1477be939174f5
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-03-20 17:15:50 +0530

    File updated at 3/20/2026, 5:15:26 PM

--- 225-ImplementStackusingQueues/225-ImplementStackusingQueues.java
+++ 225-ImplementStackusingQueues/225-ImplementStackusingQueues.java
@@ -1,32 +1,32 @@
-// Last updated: 10/21/2025, 4:05:03 PM
-class MyStack {
-    Queue<Integer> q1;
-    Queue<Integer> q2;
-
-    public MyStack() {
-        q1 = new LinkedList<>();
-        q2 = new LinkedList<>();
-    }
-
-    public void push(int x) {
-        while (!q1.isEmpty()) {
-            q2.offer(q1.poll());
-        }
-        q1.offer(x);
-        while (!q2.isEmpty()) {
-            q1.offer(q2.poll());
-        }
-    }
-
-    public int pop() {
-        return q1.poll();
-    }
-
-    public int top() {
-        return q1.peek();
-    }
-
-    public boolean empty() {
-        return q1.isEmpty();
-    }
-}
+// Last updated: 3/20/2026, 5:15:26 PM
+1class MyStack {
+2    Queue<Integer> q1;
+3    Queue<Integer> q2;
+4
+5    public MyStack() {
+6        q1 = new LinkedList<>();
+7        q2 = new LinkedList<>();
+8    }
+9
+10    public void push(int x) {
+11        while (!q1.isEmpty()) {
+12            q2.add(q1.poll());
+13        }
+14        q1.offer(x);
+15        while (!q2.isEmpty()) {
+16            q1.add(q2.poll());
+17        }
+18    }
+19
+20    public int pop() {
+21        return q1.poll();
+22    }
+23
+24    public int top() {
+25        return q1.peek();
+26    }
+27
+28    public boolean empty() {
+29        return q1.isEmpty();
+30    }
+31}
