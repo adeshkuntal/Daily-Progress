38079357f12868e3ab957d43d6f27ec3eff75ad4
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-03-25 20:38:19 +0530

    File updated at 3/25/2026, 8:38:21 PM

--- 3546-EqualSumGridPartitionI/3546-EqualSumGridPartitionI.java
+++ 3546-EqualSumGridPartitionI/3546-EqualSumGridPartitionI.java
@@ -1,40 +1,32 @@
-// Last updated: 3/25/2026, 8:37:55 PM
+// Last updated: 3/25/2026, 8:38:21 PM
 1class Solution {
 2    public boolean canPartitionGrid(int[][] grid) {
-3        long total = 0;
-4        int n = grid.length;
-5        int m = grid[0].length;
-6
-7        for(int i=0; i<n; i++){
-8            for(int j=0; j<m; j++){
+3        int n = grid.length;
+4        int m = grid[0].length;
+5
+6        long total = 0;
+7        for(int i=0;i<n;i++){
+8            for(int j=0;j<m;j++){
 9                total += grid[i][j];
 10            }
 11        }
 12
-13        int row = 0;
-14        long currRow = 0;
-15        while(row < n){
-16            for(int j=0; j<m; j++){
-17                currRow += grid[row][j];
-18            }
-19            if(total - currRow == currRow){
-20                return true;
-21            }
-22            row++;
-23        }
-24
-25        int col = 0;
-26        long currCol = 0;
-27        while(col < m){
-28            for(int j=0; j<n; j++){
-29                currCol += grid[j][col];
-30            }
-31            if(total - currCol == currCol){
-32                return true;
-33            }
-34            col++;
-35        }
-36
-37        return false;
-38    }
-39}
+13        long sum = 0;
+14        for(int i=0;i<n;i++){
+15            for(int j=0;j<m;j++){
+16                sum += grid[i][j];
+17            }
+18            if(sum * 2 == total) return true;
+19        }
+20
+21        sum = 0;
+22        for(int j=0;j<m;j++){
+23            for(int i=0;i<n;i++){
+24                sum += grid[i][j];
+25            }
+26            if(sum * 2 == total) return true;
+27        }
+28
+29        return false;
+30    }
+31}
