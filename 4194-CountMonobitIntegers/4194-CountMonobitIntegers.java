e6154f4276a5fc300e42e24fd8344556e400190f
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-03-25 22:01:29 +0530

    Create 4194-CountMonobitIntegers

---
+++ 4194-CountMonobitIntegers/4194-CountMonobitIntegers.java
@@ -0,0 +1,13 @@
+// Last updated: 3/25/2026, 10:01:31 PM
+class Solution {
+    public int countMonobit(int n) {
+        int count = 1;
+        int val = 1;
+        while(val <= n){
+            count++;
+            val  = (val << 1) | 1;
+        }
+
+        return count;
+    }
+}
