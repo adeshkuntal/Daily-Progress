f6f759b48ccb40bf62948b1be08ddd91e3509a3b
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-04-04 13:15:48 +0530

    File updated at 4/4/2026, 1:15:47 PM

--- 206-ReverseLinkedList/206-ReverseLinkedList.java
+++ 206-ReverseLinkedList/206-ReverseLinkedList.java
@@ -1,27 +1,27 @@
-// Last updated: 8/4/2025, 6:56:52 PM
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode reverseList(ListNode head) {
-        ListNode curr = head;
-        ListNode prev = null;
-
-        while(curr != null){
-            ListNode nextNode = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = nextNode;
-        }
-        head = prev;
-
-        return head;
-    }
-}
+// Last updated: 4/4/2026, 1:15:47 PM
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11
+12class Solution {
+13    public ListNode reverseList(ListNode head) {
+14        ListNode curr = head;
+15        ListNode prev = null;
+16
+17        while(curr != null){
+18            ListNode nextNode = curr.next;
+19            curr.next = prev;
+20            prev = curr;
+21            curr = nextNode;
+22        }
+23        head = prev;
+24        return head;
+25    }
+26}
