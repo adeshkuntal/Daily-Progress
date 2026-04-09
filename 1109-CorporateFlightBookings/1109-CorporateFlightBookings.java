0511fe7430aaccc4dbe8ca64d5e87fe2acfb1d67
Adesh Kuntal <142567374+adeshkuntal@users.noreply.github.com>
2026-04-09 11:51:44 +0530

    File updated at 4/9/2026, 11:51:45 AM

--- 1109-CorporateFlightBookings/1109-CorporateFlightBookings.java
+++ 1109-CorporateFlightBookings/1109-CorporateFlightBookings.java
@@ -1,4 +1,4 @@
-// Last updated: 4/9/2026, 11:51:32 AM
+// Last updated: 4/9/2026, 11:51:45 AM
 1class Solution {
 2    public int[] corpFlightBookings(int[][] bookings, int n) {
 3        int[] res = new int[n+1];
@@ -9,13 +9,12 @@
 8            res[f-1] += seat;
 9            res[l] += -seat;
 10        }
-11        System.out.print(Arrays.toString(res));
-12        int[] prefix = new int[n];
-13        prefix[0] = res[0];
-14        for(int i=1; i<n; i++){
-15            prefix[i] = prefix[i-1]+res[i];
-16        }
-17
-18        return prefix;
-19    }
-20}
+11        int[] prefix = new int[n];
+12        prefix[0] = res[0];
+13        for(int i=1; i<n; i++){
+14            prefix[i] = prefix[i-1]+res[i];
+15        }
+16
+17        return prefix;
+18    }
+19}
